ab69e708771d1ec6b55060e85a510ba9c439ae0b
jangsubin <eltm521@naver.com>
2025-03-04 17:34:57 +0900

    feat : Board 생성 - add(Pawn) 구현 - size() 구현 - findPawn() 구현

---
+++ src/main/java/Board.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private List<Pawn> pawnList;
+
+    public Board() {
+        pawnList = new ArrayList<>();
+    }
+
+    public void add(Pawn pawn) {
+        pawnList.add(pawn);
+    }
+
+    public int size() {
+        return pawnList.size();
+    }
+
+    public Pawn findPawn(int index) {
+        return pawnList.get(index);
+    }
+}
