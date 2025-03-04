671c03430b5d89137c1a7c2d2d78a1d64a8122a9
2jiyong <jdragon2000@naver.com>
2025-03-04 15:35:32 +0900

    테스트 케이스에 verifyPawn 메서드 추가하고, 테스트 케이스에 맞춰 폰 클래스 생성 완료

--- src/test/java/org/chess/PawnTest.java
+++ src/test/java/org/chess/PawnTest.java
@@ -1,7 +1,18 @@
 package org.chess;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
+    @Test
+    @DisplayName("흰색 폰이 생성되어야한다")
+    public void create() {
+        verifyPawn("white");
+        verifyPawn("black");
+    }
 
+    void verifyPawn(final String color){
+        Pawn pawn = new Pawn(color);
+        assertEquals(color,pawn.getColor());
+    }
 }
