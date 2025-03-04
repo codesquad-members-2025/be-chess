4a551fa234539fe387a838cdfbf7300d95569f18
jaehoonchoi <eeoos8840@gmail.com>
2025-03-04 20:48:49 +0900

    Pawn 클래스 생성하여 생성자로 객체를 생성할 때만 색상을 지정하도록 강제하였고, 값이 변하지 않게 final을 붙임. 다음으로 color 필드에 대한 getter 생성. “white”, “black” 문자열 값을 create() 메서드에 로컬 변수로 선언하고, Pawn 생성과 결과 값 확인하는 부분을 verifyPawn 메서드로 묶어 검은색 폰과 흰색 폰 색깔 검증 확인.

--- app/src/test/java/org/example/PawnTest.java
+++ app/src/test/java/org/example/PawnTest.java
@@ -5,12 +5,18 @@ import static org.assertj.core.api.Assertions.*;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색 폰과 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+
+        final String black = "black";
+        final String white = "white";
+
+        verifyPawn(black);
+        verifyPawn(white);
     }
 
-    // brew로 Gradle을 설치한 후, 작업 디렉토리에서 gradle init을 실행하여 Gradle 프로젝트 설정.
-    // build.gradle에 assertj 의존성 추가하여 테스트 코드 실패하는 것 확인.
+    private void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
 }
