a6348bd148ee68c210ad365b6da270e5d22d0975
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-04 20:03:46 +0900

    feat: PawnTest 테스트 클래스 구현

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
+
+    final String COLOR_W = "white";
+    final String COLOR_B = "black";
+
+    @Test
+    @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
+    public void create() {
+        verifyPawn(COLOR_W);
+        verifyPawn(COLOR_B);
+    }
+
+    public void verifyPawn(String color) {
+        Pawn whitePawn = new Pawn(color);
+        assertThat(whitePawn.getColor()).isEqualTo(color);
+    }
+
 }
