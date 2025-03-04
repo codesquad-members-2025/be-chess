e79f4843881401f3d890295293c0d263dd061c20
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-04 20:03:24 +0900

    feat: Pawn 클래스 구현

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Pawn {
+    private final String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public void move() {
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
