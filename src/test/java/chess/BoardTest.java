0f5562df9d50117ee0f74319bedbf9aac6e8bb66
jangsubin <eltm521@naver.com>
2025-03-04 17:40:54 +0900

    refactor : BoardTest 리팩토링 - @BeforeEach를 사용하여 공통으로 사용하는 변수 초기화 - verifyBoard()에서 체스판 검증하도록 수정

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -3,23 +3,32 @@ package chess;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import chess.pieces.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+    private Board board;
+    private String[] colors;
+
+    @BeforeEach
+    public void init() {
+        board = new Board();
+        colors = new String[]{Pawn.WHITE_COLOR, Pawn.BLACK_COLOR};
+    }
     @Test
     public void create() throws Exception {
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        for (int i = 0; i < colors.length; i++) {
+            verifyBoard(i);
+        }
 
 //        board.add(new Integer(7));
+    }
+
+    private void verifyBoard(int i) {
+        Pawn pawn = new Pawn(colors[i]);
+        board.add(pawn);
+
+        assertEquals(i +1, board.size());
+        assertEquals(pawn, board.findPawn(i));
     }
 }
