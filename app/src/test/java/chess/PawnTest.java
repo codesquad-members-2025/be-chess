0ac3572c0ef96b3e1a82f97c016f1ac0bafe7487
dorkem <chlwogur1000@gmail.com>
2025-03-04 16:58:58 +0900

    :recycle: DisplayName을 변경하고, 변하지않도록 final로 white, black String local variable 생성후, verifyPawn을 통해 폰 생성등 중복을 제거

--- app/src/test/java/chess/PawnTest.java
+++ app/src/test/java/chess/PawnTest.java
@@ -6,9 +6,15 @@ import static org.assertj.core.api.Assertions.*;
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        final String white = "white";
+        final String black = "black";
+        verifyPawn(white);
+        verifyPawn(black);
+    }
+    private void verifyPawn(final String color){
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
