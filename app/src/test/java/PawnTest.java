4cc88d8b28868f6189d0fcf13387131d489a03b4
moongyuxx <gyuwon822@gmail.com>
2025-03-04 17:22:51 +0900

    verifyPawn(final String color) 메소드 추가해 중복 로직 제거

--- app/src/test/java/PawnTest.java
+++ app/src/test/java/PawnTest.java
@@ -7,12 +7,11 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
-        String white = "white";
-        String black = "black";
-        Pawn pawn1 = new Pawn(white);
-        assertThat(pawn1.getColor()).isEqualTo(white);  // AssertJ 사용
-
-        Pawn pawn2 = new Pawn(black);
-        assertThat(pawn2.getColor()).isEqualTo(black);
+        verifyPawn("white");
+        verifyPawn("black");
+    }
+    public void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
     }
 }
