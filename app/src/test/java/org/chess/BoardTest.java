23c4fc58166c24e08b428df84230d1741a9a6d9a
jaehoonchoi <eeoos8840@gmail.com>
2025-03-05 18:26:34 +0900

    Chore: 불필요한 주석 제거
    중복 제거 전 코드, 컴파일 에러 코드 주석 제거

--- app/src/test/java/org/chess/BoardTest.java
+++ app/src/test/java/org/chess/BoardTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.chess.pieces.Pawn.*;
 
@@ -23,27 +21,7 @@ public class BoardTest {
 
         verifyBoard(WHITE, 1, 0);
         verifyBoard(BLACK, 2, 1);
-
-//        Pawn white = new Pawn(WHITE);
-//        board.add(white);
-//        assertThat(board.size()).isEqualTo(1);
-//        assertThat(white).isEqualTo(board.findPawn(0));
-//
-//        Pawn black = new Pawn(BLACK);
-//        board.add(black);
-//        assertThat(board.size()).isEqualTo(2);
-//        assertThat(black).isEqualTo(board.findPawn(1));
     }
-
-    /*@Test
-    @DisplayName("체스판에 Pawn 이외의 객체가 추가되지 않는지 테스트")
-    public void 체스판엔_Pawn만() {
-        Board board = new Board();
-
-        board.add(new Integer(7));
-        board.add(new ArrayList<>());
-        board.add(new int[]{1,2,3});
-    }*/
 
     private void verifyBoard(String color, int size, int index) {
         Pawn pawn = new Pawn(color);
