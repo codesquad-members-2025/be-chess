640e1129f30260b549f031953be8c48fa2c95888
jang-jinuk <dlsndifn98@gmail.com>
2025-03-05 12:17:22 +0900

    Board 클래스 생성
    - Pawn 인스턴스를 리스트로 형태로 저장하는 pawns 필드 추가

---
+++ app/src/main/java/Board.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private List<Pawn> pawns;
+
+    public Board() {
+        this.pawns = new ArrayList<>();
+    }
+}
