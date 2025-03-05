b79d764ecca3223cea05d46e1b624c6db60b8673
jaehoonchoi <eeoos8840@gmail.com>
2025-03-05 11:44:08 +0900

    Pawn 클래스에 기본 생성자를 만들어 Pawn 객체를 생성할 때 인자에 color를 넣지 않았다면 기본적으로 white 색상을 가지게 함. white와 black 문자열의 지속적인 중복으로 Pawn 클래스에 BLACK, WHITE 상수 만들어서 사용.

--- app/src/main/java/org/example/Pawn.java
+++ app/src/main/java/org/example/Pawn.java
@@ -1,12 +1,18 @@
 package org.example;
 
 public class Pawn {
+
+    public static final String BLACK = "black";
+    public static final String WHITE = "white";
+
     private final String color;
 
     public Pawn(String color) {
         this.color = color;
     }
-
+    public Pawn() {
+        this.color = WHITE;
+    }
     public String getColor() {
         return color;
     }
