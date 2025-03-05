bb06c708f97e0fa7ff1c49b01a62257e0234c29d
jang-jinuk <dlsndifn98@gmail.com>
2025-03-05 11:31:38 +0900

    Pawn 클래스 기본 생성자 추가
    Pawn 기본 생성시 color 필드를 white로 초기화하도록 구현하였습니다.

--- app/src/main/java/Pawn.java
+++ app/src/main/java/Pawn.java
@@ -1,6 +1,10 @@
 public class Pawn {
     private final String color;
 
+    public Pawn() {
+        this.color = "white";
+    }
+
     public Pawn(String color) {
         this.color = color;
     }
