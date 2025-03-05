fc26eec7ef93f79cf3cf9054dcd3bdd24da018cb
jang-jinuk <dlsndifn98@gmail.com>
2025-03-05 11:42:14 +0900

    색상 상수 추가
    - WITHE_COLOR, BLACK_COLOR 상수를 추가하였습니다.
    - 색상 문자열 중복 제거하였습니다.

--- app/src/main/java/Pawn.java
+++ app/src/main/java/Pawn.java
@@ -1,8 +1,10 @@
 public class Pawn {
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
     private final String color;
 
     public Pawn() {
-        this.color = "white";
+        this.color = WHITE_COLOR;
     }
 
     public Pawn(String color) {
