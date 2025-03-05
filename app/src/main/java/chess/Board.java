a3a3428c7eda4e2de7434721377f763c8a5b8f13
dorkem <chlwogur1000@gmail.com>
2025-03-05 12:39:06 +0900

    :recycle: Board Class 중복제거
    if, else if, else 이렇게 여러개 나오는게 말의 종류에 따라 코드가 길어질 것 같아서 따로 함수를 만들었습니다.

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -23,32 +23,32 @@ public class Board {
         for (int i = 0; i < MAX_ROW; i++) {
             List<Object> row = new ArrayList<>();
             for (int j = 0; j < MAX_CAL; j++) {
-                if ( i == 1 ){
-                    row.add(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
-                } else if ( i == 6 ){
-                    row.add(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
-                } else {
-                    row.add(" . ");
-                }
+                row.add(SetUpBoard(i));
             }
             board.add(row);
         }
     }
 
+    private Object SetUpBoard(int i){
+        if ( i == 1 ) return new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        if ( i == 6 ) return new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+        return " . ";
+    }
+
     public String getWhitePawnsResult(){
-        StringBuilder whiteLine = new StringBuilder();
-        for (Object obj : board.get(1)) {
-            whiteLine.append(((Pawn)obj).getRepresentation());
-        }
-        return whiteLine.toString();
+        return getPawnsResult(1);
     }
 
     public String getBlackPawnsResult(){
-        StringBuilder blackLine = new StringBuilder();
-        for (Object obj : board.get(6)) {
-            blackLine.append(((Pawn)obj).getRepresentation());
+        return getPawnsResult(6);
+    }
+
+    private String getPawnsResult(int index){
+        StringBuilder result = new StringBuilder();
+        for (Object obj : board.get(index)) {
+            result.append(((Pawn)obj).getRepresentation());
         }
-        return blackLine.toString();
+        return result.toString();
     }
 
     Pawn findPawn(int num){
