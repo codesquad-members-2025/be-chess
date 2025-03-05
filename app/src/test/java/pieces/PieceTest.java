2f8811cf54c748643f8c07f262325403cb5818fe
dorkem <chlwogur1000@gmail.com>
2025-03-05 20:03:37 +0900

    :sparkles: 팩토리 메소드 생성
    더 생성할 수 있으나 현재까지는 블랙, 화이트 폰만 생성하였습니다.

---
+++ app/src/test/java/pieces/PieceTest.java
@@ -0,0 +1,17 @@
+package pieces;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PieceTest {
+    @Test
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+    }
+
+    private void verifyPiece(final Piece piece, final String color, final char representation) {
+        assertEquals(color, piece.getColor());
+        assertEquals(representation, piece.getRepresentation());
+    }
+}
