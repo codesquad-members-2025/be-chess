70ab6d5c8ca095596a4f0e175267f8d04fd760ca
2jiyong <jdragon2000@naver.com>
2025-03-05 14:40:35 +0900

    Board 클래스에 메서드 추가, 테스트 통과 Board 클래스에 흰색 폰과 검은색 폰 관리하기 위한 멤버와 메서드 추가

--- src/main/java/org/chess/Board.java
+++ src/main/java/org/chess/Board.java
@@ -1,24 +1,54 @@
 package org.chess;
 
 import org.pieces.Pawn;
+
 import java.util.*;
 
 public class Board {
-    private List<Pawn> pieceList;
+    private List<Pawn> pieceList = new ArrayList<>();
+    private List<Pawn> whitePawnList = new ArrayList<>();
+    private List<Pawn> blackPawnList = new ArrayList<>();
 
-    public Board(){
+    public Board() {
         pieceList = new ArrayList<>();
     }
 
-    public void add(Pawn pawn){
+    public void add(Pawn pawn) {
         pieceList.add(pawn);
     }
 
-    public Pawn findPawn(int number){
+    public Pawn findPawn(int number) {
         return pieceList.get(number);
     }
 
-    public int size(){
+    public int size() {
         return pieceList.size();
+    }
+
+    public void initialize() {
+        for(int i = 0 ; i<8; i++){
+            Pawn pawn = new Pawn(Pawn.WHITE_COLOR);
+            whitePawnList.add(pawn);
+        }
+        for(int i = 0 ; i<8; i++){
+            Pawn pawn = new Pawn(Pawn.BLACK_COLOR);
+            blackPawnList.add(pawn);
+        }
+    }
+
+    public String getWhitePawnsResult(){
+        StringBuilder sb = new StringBuilder();
+        for(Pawn pawn : whitePawnList){
+            sb.append(pawn.getRepresentation());
+        }
+        return sb.toString();
+    }
+
+    public String getBlackPawnsResult(){
+        StringBuilder sb = new StringBuilder();
+        for(Pawn pawn : blackPawnList){
+            sb.append(pawn.getRepresentation());
+        }
+        return sb.toString();
     }
 }
