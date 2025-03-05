cd513c4f765bcb68a1dd93248c072a352ed71fdd
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-05 11:53:52 +0900

    feat: Pawn 클래스에 기본 생성자 추가

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,5 +1,12 @@
 public class Pawn {
     private final String color;
+    final String COLOR_W = "white";
+    final String COLOR_B = "black";
+
+    // 기본생성자
+    public Pawn() {
+        this.color = "white";
+    }
 
     public Pawn(String color) {
         this.color = color;
