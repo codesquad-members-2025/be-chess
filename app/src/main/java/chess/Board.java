f98577d942f149fe0b28d794496014d7eccaae04
dorkem <chlwogur1000@gmail.com>
2025-03-05 20:04:25 +0900

    집가서 구현
    집에서 구현하기 위해 추가했습니다.

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -1,36 +1,40 @@
 package chess;
 
-import pieces.Pawn;
-
+import pieces.Piece;
+import utils.StringUtils;
 import java.util.*;
 
 public class Board {
-    private List<Pawn> pawns = new ArrayList<>();
+    private List<Piece> pieces = new ArrayList<>();
     public final static int MAX_ROW = 8;
     public final static int MAX_CAL = 8;
     private Object[][] board = new Object[MAX_ROW][MAX_CAL];
 
-    public void add(Pawn pawn) {
-        pawns.add(pawn);
+    public void add(Piece piece) {
+        pieces.add(piece);
     }
 
     public int size(){
-        return pawns.size();
+        return pieces.size();
     }
 
     public void initialize(){
         for (int i = 0; i < MAX_ROW; i++) {
             for (int j = 0; j < MAX_CAL; j++) {
-                board[i][j] = SetUpBoard(i);
+//                board[i][j] = SetUpBoard(i);
             }
         }
     }
 
-    private Object SetUpBoard(int i){
-        if ( i == 1 ) return new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
-        if ( i == 6 ) return new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        return ".";
+    public void pieceCount(){
+
     }
+
+//    private Object SetUpBoard(int i){
+//        if ( i == 1 ) return new Piece(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION);
+//        if ( i == 6 ) return new Piece(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION);
+//        return ".";
+//    }
 
     public String getWhitePawnsResult(){
         return getPawnsResult(6);
@@ -43,13 +47,13 @@ public class Board {
     private String getPawnsResult(int index){
         StringBuilder result = new StringBuilder();
         for (Object obj : board[index]) {
-            result.append(((Pawn)obj).getRepresentation());
+            result.append(((Piece)obj).getRepresentation());
         }
         return result.toString();
     }
 
-    Pawn findPawn(int num){
-        return pawns.get(num);
+    Piece findPawn(int num){
+        return pieces.get(num);
     }
 
     public void print(){
@@ -57,14 +61,16 @@ public class Board {
 
         for (Object[] row : board) {
             for (Object obj : row) {
-                if (obj instanceof Pawn) {
-                    result.append(((Pawn)obj).getRepresentation());
+                if (obj instanceof Piece) {
+                    result.append(((Piece)obj).getRepresentation());
                 } else {
                     result.append(obj);
                 }
             }
-            result.append("\n");
+            result.append(StringUtils.NEWLINE);
         }
         System.out.println(result);
     }
+
+
 }
--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -2,35 +2,28 @@ package chess;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-
-import pieces.Pawn;
+import static utils.StringUtils.appendNewLine;
+import pieces.Piece;
 
 public class BoardTest {
     private Board board;
 
     @BeforeEach
-    void init(){
+    void setup(){
         board = new Board();
     }
 
     @Test
     public void create() throws Exception {
-        addAndVerifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION, 0);
-        addAndVerifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION, 1);
-    }
-
-    @Test
-    public void initialize() throws Exception {
-        Board board = new Board();
         board.initialize();
-        assertEquals("pppppppp", board.getWhitePawnsResult());
-        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
-    }
-
-    private void addAndVerifyPawn(final String color, final char representation, int expectedNum){
-        Pawn pawn = new Pawn(color, representation);
-        board.add(pawn);
-        assertEquals(expectedNum+1, board.size());
-        assertEquals(pawn, board.findPawn(expectedNum));
+//        assertEquals(32, board.pieceCount());
+//        String blankRank = appendNewLine("........");
+//        assertEquals(
+//            appendNewLine("RNBQKBNR") +
+//            appendNewLine("PPPPPPPP") +
+//            blankRank + blankRank + blankRank + blankRank +
+//            appendNewLine("pppppppp") +
+//            appendNewLine("rnbqkbnr"),
+//            board.showBoard());
     }
 }
