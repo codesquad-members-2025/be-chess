6028d12eb8029054bc436f68a238e14b267ac34c
jaehoonchoi <eeoos8840@gmail.com>
2025-03-05 12:35:43 +0900

    Test: @DisplayName 어노테이션 추가
    BoardTest의 create 메서드에 @DisplayName 어노테이션 추가

--- app/src/test/java/org/example/BoardTest.java
+++ app/src/test/java/org/example/BoardTest.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class BoardTest {
     @Test
+    @DisplayName("체스판에 폰이 잘 들어가는지 테스트")
     public void create() throws Exception {
         Board board = new Board();
 
