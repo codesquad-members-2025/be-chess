021186e09faacaf6931cea492e0546d4cfa31a46
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-05 18:02:07 +0900

    refactor: 중복 제거
    JUnit5 의 @BeforeEach 를 사용해서 중복 제거

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,13 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pieces.Pawn;
 
 public class BoardTest {
+    private Board board;
+
+    // 각 테스트 전에 실행
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
     @Test
     public void create() throws Exception {
-        Board board = new Board();
-
         Pawn white = new Pawn(Pawn.COLOR_W);
         board.add(white);
         assertEquals(1, board.size());
