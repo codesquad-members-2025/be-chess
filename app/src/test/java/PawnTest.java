61a129af43256faea555f515b409e853725e1aa9
Gyuwon Moon <112413876+moongyuxx@users.noreply.github.com>
2025-03-05 17:15:50 +0900

    [BRIE] step2-1 체스 프로젝트 시작 (#9)
    * PawnTest.java 생성하고 build.gradle 수정 -> JUnit & AssertJ추ê°가
    
    * PawnTest.java에 두 번째 PAWN을 생성하고, black인ì§ 검증
    
    * "white", "black" 문자에 대한 중복을 제거하기 위해 create() 메소드에 local variable을 추가
    
    * verifyPawn(final String color) 메소드 추가해 중복 로직 제거

---
+++ app/src/test/java/PawnTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() {
+        verifyPawn("white");
+        verifyPawn("black");
+    }
+    public void verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
+}
