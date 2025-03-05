099a20f059be86de5150d4e1892c561d86e82bb8
2jiyong <jdragon2000@naver.com>
2025-03-05 10:56:34 +0900

    Board 클래스 생성하고, 테스트 코드에 있는 메서드 생성

---
+++ src/main/java/org/chess/Board.java
@@ -0,0 +1,24 @@
+package org.chess;
+
+import org.chess.pieces.Pawn;
+import java.util.*;
+
+public class Board {
+    private List<Pawn> pieceList;
+
+    public Board(){
+        pieceList = new ArrayList<>();
+    }
+
+    public void add(Pawn pawn){
+        pieceList.add(pawn);
+    }
+
+    public Pawn findPawn(int number){
+        return pieceList.get(number);
+    }
+
+    public int size(){
+        return pieceList.size();
+    }
+}
