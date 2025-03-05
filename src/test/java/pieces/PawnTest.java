693be5499ed9bb9ecafc989cedf31fa3346f859b
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-05 18:01:04 +0900

    refactor: 파일 재배치
    pieces 패키지 생성

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -1,5 +1,6 @@
+package pieces;
+
 import org.junit.jupiter.api.*;
-import pieces.Pawn;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
