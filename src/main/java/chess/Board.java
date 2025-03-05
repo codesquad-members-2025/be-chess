aecb3c2a1aa2dea65b9a7c09bd94425f18d81d96
jangsubin <eltm521@naver.com>
2025-03-05 17:59:41 +0900

    feat : 보드 초기화시 전체 말 올라가도록 구현 - size() -> pieceList()로 변경 - showBoard() 전체 보드판 문자열 리턴

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -18,7 +18,7 @@ public class Board {
         pieceList.add(piece);
     }
 
-    public int size() {
+    public int pieceCount() {
         return pieceList.size();
     }
 
@@ -29,7 +29,11 @@ public class Board {
     public void initialize() {
         initializeEmptyBoard();
         addPawnToList();
+        addPieceToList();
         addPawnToBoard();
+        addBlackPiecesToBoard();
+        addWhitePiecesToBoard();
+
     }
 
     public String getPawnResult(String color){
@@ -51,17 +55,63 @@ public class Board {
         System.out.println(sb);
     }
 
+    public String showBoard() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chessBoard.length; i++) {
+            for (int j = 0; j < chessBoard.length; j++) {
+                sb.append(chessBoard[i][j]);
+            }
+            sb.append(StringUtils.appendNewLine(""));
+        }
+        return sb.toString();
+    }
+
+    private void addWhitePiecesToBoard() {
+        chessBoard[7][0] = Piece.WHITE_ROOK_REPRESENTATION;
+        chessBoard[7][1] = Piece.WHITE_KNIGHT_REPRESENTATION;
+        chessBoard[7][2] = Piece.WHITE_BISHOP_REPRESENTATION;
+        chessBoard[7][3] = Piece.WHITE_QUEEN_REPRESENTATION;
+        chessBoard[7][4] = Piece.WHITE_KING_REPRESENTATION;
+        chessBoard[7][5] = Piece.WHITE_BISHOP_REPRESENTATION;
+        chessBoard[7][6] = Piece.WHITE_KNIGHT_REPRESENTATION;
+        chessBoard[7][7] = Piece.WHITE_ROOK_REPRESENTATION;
+    }
+
+    private void addBlackPiecesToBoard() {
+        chessBoard[0][0] = Piece.BLACK_ROOK_REPRESENTATION;
+        chessBoard[0][1] = Piece.BLACK_KNIGHT_REPRESENTATION;
+        chessBoard[0][2] = Piece.BLACK_BISHOP_REPRESENTATION;
+        chessBoard[0][3] = Piece.BLACK_QUEEN_REPRESENTATION;
+        chessBoard[0][4] = Piece.BLACK_KING_REPRESENTATION;
+        chessBoard[0][5] = Piece.BLACK_BISHOP_REPRESENTATION;
+        chessBoard[0][6] = Piece.BLACK_KNIGHT_REPRESENTATION;
+        chessBoard[0][7] = Piece.BLACK_ROOK_REPRESENTATION;
+    }
+
+    private void addPieceToList() {
+        pieceList.add(Piece.createWhiteKnight());
+        pieceList.add(Piece.createBlackKnight());
+        pieceList.add(Piece.createWhiteBishop());
+        pieceList.add(Piece.createBlackBishop());
+        pieceList.add(Piece.createWhiteRook());
+        pieceList.add(Piece.createBlackRook());
+        pieceList.add(Piece.createWhiteQueen());
+        pieceList.add(Piece.createBlackQueen());
+        pieceList.add(Piece.createWhiteKing());
+        pieceList.add(Piece.createBlackKing());
+    }
+
     private void addPawnToBoard() {
         for (int i = 0; i < chessBoard.length; i++) {
-            chessBoard[1][i] = Piece.BLACK_REPRESENTATION;
-            chessBoard[6][i] = Piece.WHITE_REPRESENTATION;
+            chessBoard[1][i] = Piece.BLACK_PAWN_REPRESENTATION;
+            chessBoard[6][i] = Piece.WHITE_PAWN_REPRESENTATION;
         }
     }
 
     private void addPawnToList() {
         for (int i = 0; i < chessBoard.length; i++) {
-            pieceList.add(new Piece());
-            pieceList.add(new Piece(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION));
+            pieceList.add(Piece.createWhitePawn());
+            pieceList.add(Piece.createBlackPawn());
         }
     }
 
