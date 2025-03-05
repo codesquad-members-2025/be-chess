9740809cbd56a57bd9407d3e3387915c0d22c07e
2jiyong <jdragon2000@naver.com>
2025-03-05 10:40:06 +0900

    Pawn 클래스에 기본 생성자 추가

--- src/main/java/org/chess/Pawn.java
+++ src/main/java/org/chess/Pawn.java
@@ -3,6 +3,10 @@ package org.chess;
 public class Pawn {
     private String color;
 
+    public Pawn(){
+        color = "white";
+    }
+
     public Pawn(String color){
         this.color = color;
     }
