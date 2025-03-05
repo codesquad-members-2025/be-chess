16d4171327f2c715ea89310c50bb10020eb4bee1
2jiyong <jdragon2000@naver.com>
2025-03-05 13:39:43 +0900

    PawnTest Presentation 확인할 수 있도록 새로 갱신

--- src/test/java/org/pieces/PawnTest.java
+++ src/test/java/org/pieces/PawnTest.java
@@ -7,22 +7,23 @@ import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
     @Test
-    @DisplayName("두 가지 폰 생성")
-    public void create() {
-        verifyPawn("white");
-        verifyPawn("black");
-    }
-
-    void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(color).isEqualTo(pawn.getColor());
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertThat(Pawn.WHITE_COLOR).isEqualTo(pawn.getColor());
+        assertThat(Pawn.WHITE_REPRESENTATION).isEqualTo(pawn.getRepresentation());
     }
 
     @Test
-    public void create_기본생성자() throws Exception {
-        Pawn pawn = new Pawn();
-        assertThat("white").isEqualTo(pawn.getColor());
+    @DisplayName("두 가지 폰 생성")
+    public void create() {
+        verifyPawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION);
     }
 
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color);
+        assertThat(color).isEqualTo(pawn.getColor());
+        assertThat(representation).isEqualTo(pawn.getRepresentation());
+    }
 }
 
