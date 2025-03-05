fd3039ede27393c3517e8fbe572ed536f8335d60
dorkem <chlwogur1000@gmail.com>
2025-03-05 12:22:00 +0900

    :sparkles: Board 초기화
    Object로 String과 Pawn, 나머지 말 객체들이 들어갈 수 있도록 Object형 List를 담는 2중 리스트를 선언하였고, 사용자에게 1(화이트 폰), 6(블랙 폰)열을 반환하는 코드를 작성하였습니다.

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -6,6 +6,9 @@ import java.util.*;
 
 public class Board {
     private List<Pawn> pawns = new ArrayList<>();
+    public final static int MAX_ROW = 8;
+    public final static int MAX_CAL = 8;
+    private List<List<Object>> board;
 
     public void add(Pawn pawn) {
         pawns.add(pawn);
@@ -13,6 +16,39 @@ public class Board {
 
     public int size(){
         return pawns.size();
+    }
+
+    public void initialize(){
+        board = new ArrayList<>();
+        for (int i = 0; i < MAX_ROW; i++) {
+            List<Object> row = new ArrayList<>();
+            for (int j = 0; j < MAX_CAL; j++) {
+                if ( i == 1 ){
+                    row.add(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
+                } else if ( i == 6 ){
+                    row.add(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
+                } else {
+                    row.add(" . ");
+                }
+            }
+            board.add(row);
+        }
+    }
+
+    public String getWhitePawnsResult(){
+        StringBuilder whiteLine = new StringBuilder();
+        for (Object obj : board.get(1)) {
+            whiteLine.append(((Pawn)obj).getRepresentation());
+        }
+        return whiteLine.toString();
+    }
+
+    public String getBlackPawnsResult(){
+        StringBuilder blackLine = new StringBuilder();
+        for (Object obj : board.get(6)) {
+            blackLine.append(((Pawn)obj).getRepresentation());
+        }
+        return blackLine.toString();
     }
 
     Pawn findPawn(int num){
--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -15,12 +15,20 @@ public class BoardTest {
 
     @Test
     public void create() throws Exception {
-        addAndVerifyPawn(Pawn.WHITE_COLOR, 0);
-        addAndVerifyPawn(Pawn.BLACK_COLOR, 1);
+        addAndVerifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION, 0);
+        addAndVerifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION, 1);
     }
 
-    private void addAndVerifyPawn(String color, int expectedNum){
-        Pawn pawn = new Pawn(color);
+    @Test
+    public void initialize() throws Exception {
+        Board board = new Board();
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+    }
+
+    private void addAndVerifyPawn(final String color, final char representation, int expectedNum){
+        Pawn pawn = new Pawn(color, representation);
         board.add(pawn);
         assertEquals(expectedNum+1, board.size());
         assertEquals(pawn, board.findPawn(expectedNum));
