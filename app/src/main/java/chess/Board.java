859d8d673fe6b9fe966482c55384091d6411b756
dorkem <chlwogur1000@gmail.com>
2025-03-06 22:07:05 +0900

    :sparkles: init() 재정의 및 함수 생성
    아예 board를 2차원 배열에서 ArrayList<Rank>로 만들었음
    
    또한 이동, 계산, 정렬, 이동 등의 메서드를 구현

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -1,96 +1,129 @@
 package chess;
 
-import pieces.Piece;
-
+import pieces.*;
+import pieces.Piece.*;
 import java.util.*;
-
 import static utils.StringUtils.appendNewLine;
 
 public class Board {
-    private List<Piece> pieces = new ArrayList<>();
-    public final static int MAX_ROW = 8;
-    public final static int MAX_CAL = 8;
-    private Object[][] board = new Object[MAX_ROW][MAX_CAL];
-    private int pieceNum = 0;
-
-//    public void initialize(){
-//        addBlackPieces();
-//        addBlackPawns();
-//        addBlankLanks();
-//        addWhitePawns();
-//        addWhitePieces();
-//    }
-
-//    public void addBlackPieces(){
-//        Object[] indexZero = board[0];
-//        indexZero[0] = Piece.createBlackRook();
-//        indexZero[1] = Piece.createBlackKnight();
-//        indexZero[2] = Piece.createBlackBishop();
-//        indexZero[3] = Piece.createBlackQueen();
-//        indexZero[4] = Piece.createBlackKing();
-//        indexZero[5] = Piece.createBlackBishop();
-//        indexZero[6] = Piece.createBlackKnight();
-//        indexZero[7] = Piece.createBlackRook();
-//        pieceNum+=8;
-//    }
-//
-//    public void addBlackPawns(){
-//        Object[] indexZero = board[1];
-//        for(int i = 0; i < MAX_CAL; i++){
-//            indexZero[i] = Piece.createBlackPawn();
-//            pieceNum++;
-//        }
-//    }
-//
-//    public void addBlankLanks(){
-//        for (int i = 2; i < 6; i++){
-//            for (int j = 0; j < MAX_CAL; j++){
-//                board[i][j] = '.';
-//            }
-//        }
-//    }
-//
-//    public void addWhitePawns(){
-//        Object[] indexZero = board[6];
-//        for(int i = 0; i < MAX_CAL; i++){
-//            indexZero[i] = Piece.createWhitePawn();
-//            pieceNum++;
-//        }
-//    }
-//
-//    public void addWhitePieces(){
-//        Object[] indexZero = board[7];
-//        indexZero[0] = Piece.createWhiteRook();
-//        indexZero[1] = Piece.createWhiteKnight();
-//        indexZero[2] = Piece.createWhiteBishop();
-//        indexZero[3] = Piece.createWhiteQueen();
-//        indexZero[4] = Piece.createWhiteKing();
-//        indexZero[5] = Piece.createWhiteBishop();
-//        indexZero[6] = Piece.createWhiteKnight();
-//        indexZero[7] = Piece.createWhiteRook();
-//        pieceNum+=8;
-//    }
-//
-//    public int pieceCount(){
-//        return pieceNum;
-//    }
-//
-//    public String showBoard(){
-//        StringBuilder answer = new StringBuilder();
-//
-//        for (Object[] row : board) {
-//            String result = "";
-//            for (Object obj : row) {
-//                if (obj instanceof Piece) {
-//                    result += ((Piece)obj).getRepresentation();
-//                } else {
-//                    result += obj;
-//                }
-//            }
-//            answer.append(appendNewLine(result));
-//        }
-//        return answer.toString();
-//    }
+    private ArrayList<Rank> pieces = new ArrayList<>();
 
 
+    public void initialize(){
+        pieces.clear();
+        pieces.add(createBlackRank());
+        pieces.add(createBlackPawn());
+        for(int i = 0; i<4; i++){
+            pieces.add(createBlank());
+        }
+        pieces.add(createWhitePawn());
+        pieces.add(createWhiteRank());
+    }
+
+    public Piece findPiece(String position){
+        //piece 객체잡고 리턴해주면 될듯
+        int x = position.charAt(0) - 'a';
+        int y = 8 - (position.charAt(1) - '0');
+        return pieces.get(y).getPiece(x);
+    }
+
+    private Rank createBlackRank(){
+        ArrayList<Piece> horses = new ArrayList<>();
+        horses.add(Piece.createBlackRook());
+        horses.add(Piece.createBlackKnight());
+        horses.add(Piece.createBlackBishop());
+        horses.add(Piece.createBlackQueen());
+        horses.add(Piece.createBlackKing());
+        horses.add(Piece.createBlackBishop());
+        horses.add(Piece.createBlackKnight());
+        horses.add(Piece.createBlackRook());
+        return new Rank(horses);
+    }
+
+    private Rank createWhiteRank(){
+        ArrayList<Piece> horses = new ArrayList<>();
+        horses.add(Piece.createWhiteRook());
+        horses.add(Piece.createWhiteKnight());
+        horses.add(Piece.createWhiteBishop());
+        horses.add(Piece.createWhiteQueen());
+        horses.add(Piece.createWhiteKing());
+        horses.add(Piece.createWhiteBishop());
+        horses.add(Piece.createWhiteKnight());
+        horses.add(Piece.createWhiteRook());
+        return new Rank(horses);
+    }
+
+    private Rank createWhitePawn(){
+        ArrayList<Piece> pawns = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            pawns.add(Piece.createWhitePawn());
+        }
+        return new Rank(pawns);
+    }
+
+    private Rank createBlackPawn(){
+        ArrayList<Piece> pawns = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            pawns.add(Piece.createBlackPawn());
+        }
+        return new Rank(pawns);
+    }
+
+    private Rank createBlank(){
+        ArrayList<Piece> blanks = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            blanks.add(Piece.createBlank());
+        }
+        return new Rank(blanks);
+    }
+
+    public void initializeEmpty(){
+        pieces.clear();
+        for(int i = 0; i<8; i++){
+            pieces.add(createBlank());
+        }
+    }
+
+    public void move(String position, Piece piece){
+        int x = position.charAt(0) - 'a';
+        int y = 8 - (position.charAt(1) - '0');
+        pieces.get(y).setPiece(x, piece);
+    }
+
+    public String showBoard(){
+        StringBuilder answer = new StringBuilder();
+
+        for (Rank rank : pieces){
+            answer.append(appendNewLine(rank.toString()));
+        }
+
+        return answer.toString();
+    }
+
+    public double caculcatePoint(Color color){
+        double score = 0.0;
+        for (Rank rank : pieces){
+            for(int i = 0; i < 8; i++){
+                Piece piece = rank.getPiece(i);
+                if (piece.getType() != Type.NO_PIECE && piece.getColor() == color){
+                    score += piece.getType().getdefaultPoint();
+                }
+            }
+        }
+        return score;
+    }
+
+    public ArrayList<Piece> sortByScore(Color color, boolean ascending){
+        ArrayList<Piece> pieceList = new ArrayList<>();
+        for (Rank rank : pieces){
+            for(int i = 0; i < 8; i++){
+                Piece piece = rank.getPiece(i);
+                if (piece.getType() != Type.NO_PIECE && piece.getColor() == color){
+                    pieceList.add(piece);
+                }
+            }
+        }
+        pieceList.sort(Comparator.comparing(p -> ascending ? p.getType().getdefaultPoint() : -p.getType().getdefaultPoint()));
+        return pieceList;
+    }
 }
