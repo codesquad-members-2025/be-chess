56442f410dbfc9cc50de3a64aea0178c108a2fa6
jangsubin <eltm521@naver.com>
2025-03-06 12:36:31 +0900

    refactor : 팩토리 메소드 리팩토링 - 중복되는 팩토리들 삭제 - 타입을 매개변수로 받아 색상별 기물을 생성하는 팩토리 구현

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -47,47 +47,16 @@ public class Piece {
     private Piece() {
     }
 
-    public static Piece createWhitePawn() {
-        return new Piece(Type.PAWN, Color.WHITE);
-    }
-    public static Piece createBlackPawn() {
-        return new Piece(Type.PAWN, Color.BLACK);
-    }
-    public static Piece createWhiteKnight() {
-        return new Piece(Type.KNIGHT, Color.WHITE);
-    }
-    public static Piece createBlackKnight() {
-        return new Piece(Type.KNIGHT, Color.BLACK);
-    }
-    public static Piece createWhiteRook() {
-        return new Piece(Type.ROOK, Color.WHITE);
-    }
-    public static Piece createBlackRook() {
-        return new Piece(Type.ROOK, Color.BLACK);
-    }
-    public static Piece createWhiteBishop() {
-        return new Piece(Type.BISHOP, Color.WHITE);
-    }
-    public static Piece createBlackBishop() {
-        return new Piece(Type.BISHOP, Color.BLACK);
-    }
-    public static Piece createWhiteQueen() {
-        return new Piece(Type.QUEEN, Color.WHITE);
-    }
-    public static Piece createBlackQueen() {
-        return new Piece(Type.QUEEN, Color.BLACK);
-    }
-    public static Piece createWhiteKing() {
-        return new Piece(Type.KING, Color.WHITE);
-    }
-    public static Piece createBlackKing() {
-        return new Piece(Type.KING, Color.BLACK);
-    }
-
     public static Piece createBlank() {
         return new Piece(Type.NO_PIECE, Color.NOCOLOR);
     }
 
+    public static Piece createWhite(Type type) {
+        return new Piece(type, Color.WHITE);
+    }
+    public static Piece createBlack(Type type) {
+        return new Piece(type, Color.BLACK);
+    }
     public Type getName() {
         return name;
     }
