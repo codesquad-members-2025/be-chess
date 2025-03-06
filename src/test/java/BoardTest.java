31348e006730cf0445d701de1e952ddb456160fb
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-06 12:16:24 +0900

    refactor: BoardTest 수정
    BoardTest 테스트 내용 수정

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -2,7 +2,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import pieces.Pawn;
 
 public class BoardTest {
     private Board board;
@@ -12,16 +11,11 @@ public class BoardTest {
     void setUp() {
         board = new Board();
     }
-    @Test
-    public void create() throws Exception {
-        Pawn white = new Pawn(Pawn.COLOR_W);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(Pawn.COLOR_W, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.COLOR_B);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(Pawn.COLOR_B, board.findPawn(1));
+    @Test
+    public void initialize() throws Exception {
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnResult());
     }
 }
