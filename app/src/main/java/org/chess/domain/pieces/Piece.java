dc981f485a8e4ddc3316a3fe0624267f5e79528f
gladhee <gladhee@icloud.com>
2025-03-06 15:39:01 +0900

    refactor(Piece): remove name field

--- app/src/main/java/org/chess/domain/pieces/Piece.java
+++ app/src/main/java/org/chess/domain/pieces/Piece.java
@@ -2,14 +2,6 @@ package org.chess.domain.pieces;
 
 public class Piece {
 
-    public static final String PAWN_NAME = "pawn";
-    public static final String QUEEN_NAME = "queen";
-    public static final String KING_NAME = "king";
-    public static final String ROOK_NAME = "rook";
-    public static final String BISHOP_NAME = "bishop";
-    public static final String KNIGHT_NAME = "knight";
-    public static final String BLANK_NAME = "blank";
-
     public enum Color {
         WHITE("white"), BLACK("black"), NOCOLOR("null");
 
@@ -52,26 +44,24 @@ public class Piece {
         }
     }
 
-    private final String name;
     private final Color color;
     private final Type representation;
 
-    private Piece(String name, Color color, Type representation) {
-        this.name = name;
+    private Piece(Color color, Type representation) {
         this.color = color;
         this.representation = representation;
     }
 
     public static Piece createWhite(Piece.Type type) {
-        return new Piece(type.name(), Color.WHITE, type);
+        return new Piece(Color.WHITE, type);
     }
 
     public static Piece createBlack(Piece.Type type) {
-        return new Piece(type.name(), Color.BLACK, type);
+        return new Piece(Color.BLACK, type);
     }
 
     public static Piece createBlankPiece() {
-        return new Piece(BLANK_NAME, Color.NOCOLOR, Type.NO_PIECE);
+        return new Piece(Color.NOCOLOR, Type.NO_PIECE);
     }
 
     public boolean isWhite() {
@@ -80,10 +70,6 @@ public class Piece {
 
     public boolean isBlack() {
         return color.equals(Color.BLACK);
-    }
-
-    public String getName() {
-        return name;
     }
 
     public Color getColor() {
