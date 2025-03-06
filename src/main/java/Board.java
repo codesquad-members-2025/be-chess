b9e2d5f7270b90cf8b29a9c33aa82972c55f5763
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-06 12:15:18 +0900

    refactor: Board 수정
    Board 에 새로운 기능 구현

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,85 +1,74 @@
 import pieces.Pawn;
-import pieces.Piece;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
 
-    private final char[][] board = new char[8][8];
+    private final List<String> BOARD = new ArrayList<>();
+    private final int WHITE_PAWN_INDEX = 6;
+    private final int BLACK_PAWN_INDEX = 1;
+    private final int BOARD_ROW_SIZE = 8;
 
-//    public void startGame() {
-//        List<Pawn> whitePawnList = makeWhitePawnList();
-//        List<Pawn> blackPawnList = makeBlackPawnList();
-//        initialize(whitePawnList, blackPawnList);
-//    }
+    // BOARD 빈칸으로 초기화
+    public Board() {
+        String empty = "........";
+        for (int i = 0; i < 8; i++) {
+            BOARD.add(empty);
+        }
+    }
 
     // 처음에 시작하면, 흰색 폰을 8개 생성해서 List 에 넣고, 검은색 폰을 8개 생성해서 List 에 넣고
-    private void initialize() {
-        StringBuilder builder = new StringBuilder();
+    public void initialize() {
         List<Pawn> whitePawnList = makeWhitePawnList();
         List<Pawn> blackPawnList = makeBlackPawnList();
-
-
-        print(board);
+        String whitePawnString = createPawnString(whitePawnList);
+        String blackPawnString = createPawnString(blackPawnList);
+        BOARD.set(WHITE_PAWN_INDEX, whitePawnString);
+        BOARD.set(BLACK_PAWN_INDEX, blackPawnString);
+        print();
     }
 
-    private void print(StringBuilder builder) {
-
-    }
-
-    public String getWhitePawnResult() {
-        StringBuilder builder = new StringBuilder();
-        int whitePawnIndex = 6;
-
-
-    }
-
-    public String getBlackPawnResult() {
-        StringBuilder builder = new StringBuilder();
-        int blackPawnIndex = 1;
-        for (int i = 0; i < 8; i++) {
-
+    private void print() {
+        for (String line : BOARD) {
+            System.out.println(line);
         }
-
     }
 
+    // BOARD 에서 흰색 Pawn 줄 반환
+    public String getWhitePawnResult() {
+        return BOARD.get(WHITE_PAWN_INDEX);
+    }
+
+    // BOARD 에서 검은색 Pawn 줄 반환
+    public String getBlackPawnResult() {
+        return BOARD.get(BLACK_PAWN_INDEX);
+    }
+
+    // 흰색 Pawn 의 List 반환
     private List<Pawn> makeWhitePawnList() {
         List<Pawn> whitePawnList = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < BOARD_ROW_SIZE; i++) {
             whitePawnList.add(new Pawn());
         }
         return whitePawnList;
     }
 
+    // 검은색 Pawn 의 List 반환
     private List<Pawn> makeBlackPawnList() {
         List<Pawn> blackPawnList = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < BOARD_ROW_SIZE; i++) {
             blackPawnList.add(new Pawn(Pawn.COLOR_BLACK, Pawn.REPRESENTATION_BLACK));
         }
         return blackPawnList;
     }
 
-//    public void add(Piece piece) {
-//        board.add(piece);
-//    }
-
-//    public int size() {
-//        return board.size();
-//    }
-//
-//    // count 번째로 들어가 있는 Pawn 의 색 반환
-//    public String findPawn(int index) {
-//        String answer = "";
-//        int count = 0;
-//        while (count <= index) {
-//            for (Piece piece : board) {
-//                if (piece instanceof Pawn) {
-//                    answer = piece.getColor();
-//                    count++;
-//                }
-//            }
-//        }
-//        return answer;
-//    }
+    // Pawn 의 List 의 내용을 StringBuilder 에 담고, String 으로 반환
+    private String createPawnString(List<Pawn> pawnList) {
+        StringBuilder builder = new StringBuilder();
+        for (Pawn pawn : pawnList) {
+            builder.append(pawn.getRepresentation());
+        }
+        return builder.toString();
+    }
 }
