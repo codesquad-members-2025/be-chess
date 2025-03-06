b3b7e5c79a7b3dd6a2616096123ffa940dd1454b
jaehoonchoi <eeoos8840@gmail.com>
2025-03-06 15:37:43 +0900

    Test: PieceTest 코드 추가
    6개의 기물들이 잘 생성되는지 테스트

--- app/src/test/java/chess/pieces/PieceTest.java
+++ app/src/test/java/chess/pieces/PieceTest.java
@@ -4,20 +4,27 @@ import static org.assertj.core.api.Assertions.*;
 
 public class PieceTest {
     @Test
-    public void create_기본생성자() throws Exception {
-        Piece piece = new Piece();
-        assertThat(piece.getColor()).isEqualTo(Piece.WHITE);
-        assertThat(piece.getRepresentation()).isEqualTo(Piece.WHITE_REPRESENTATION);
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
     }
 
-    @Test
-    public void create() {
-        verifyPawn(Piece.WHITE, Piece.WHITE_REPRESENTATION);
-        verifyPawn(Piece.BLACK, Piece.BLACK_REPRESENTATION);
-    }
-
-    void verifyPawn(final String color, final char representation) {
-        Piece piece = new Piece(color, representation);
+    private void verifyPiece(final Piece piece, final String color, final char representation) {
         assertThat(piece.getColor()).isEqualTo(color);
         assertThat(piece.getRepresentation()).isEqualTo(representation);
     }
