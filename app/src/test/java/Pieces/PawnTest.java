5d45106cb937f10b21bf0ba79f1ca2aa0fd94f83
jang-jinuk <dlsndifn98@gmail.com>
2025-03-06 10:44:03 +0900

    create 테스트 메서드 검증 항목 추가
    생성한 폰이 색상마다 정해진 기호와 일치한지 확인하는 로직을 추가하였습니다.

--- app/src/test/java/Pieces/PawnTest.java
+++ app/src/test/java/Pieces/PawnTest.java
@@ -11,8 +11,8 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 또는 검은색 폰이 생성되어야 한다")
     public void create() {
-        verifyPawn(Pawn.WHITE_COLOR);
-        verifyPawn(Pawn.BLACK_COLOR);
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
     @Test
@@ -22,8 +22,9 @@ public class PawnTest {
         assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
     }
 
-    void verifyPawn(final String color) {
+    void verifyPawn(final String color, final char representation) {
         Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor().equals(color));
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation);
     }
 }
