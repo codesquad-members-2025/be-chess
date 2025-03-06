f6e9e869f9ae5a505004d56a00d0b19e81bc213d
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-06 10:59:57 +0900

    refactor: Pawn 클래스 수정
    Pawn 객체가 색깔에 따른 char 값을 갖도록 함

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -1,20 +1,30 @@
 package pieces;
 
 public class Pawn implements Piece {
-    public static final String COLOR_W = "white";
-    public static final String COLOR_B = "black";
+    public static final String COLOR_WHITE = "white";
+    public static final String COLOR_BLACK = "black";
+    public static final char REPRESENTATION_WHITE = 'p';
+    public static final char REPRESENTATION_BLACK = 'P';
+
     private final String color;
+    private final char representation;
 
     // 기본생성자
     public Pawn() {
-        this.color = COLOR_W;
+        this.color = COLOR_WHITE;
+        this.representation = REPRESENTATION_WHITE;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 }
