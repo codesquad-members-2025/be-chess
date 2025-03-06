5e49c1df74bfe7334a85222134d45f5fb7dd88b5
jaehoonchoi <eeoos8840@gmail.com>
2025-03-06 12:13:02 +0900

    Feat: main 구현
    Chess 클래스를 생성하여 체스판을 초기화하고 end를 입력할 때까지 사용자의 입력을 받는 main 메서드 생성

---
+++ app/src/main/java/chess/Chess.java
@@ -0,0 +1,25 @@
+package chess;
+
+import java.util.Scanner;
+
+public class Chess {
+
+    private static final String END = "end";
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Board board = new Board();
+        System.out.println("게임을 시작합니다. 종료를 원하신다면 end 를 입력해주세요.");
+
+        board.initialize();
+        while (true) {
+            String input = sc.nextLine().trim();
+            if (input.equals(END)) {
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
+        }
+        sc.close();
+    }
+}
