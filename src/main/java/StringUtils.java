60429a5b26be5789b6e9d425aea7812bd1ca2a03
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-06 16:48:00 +0900

    feat: StringUtils 클래스 생성
    개행의 유틸리티 메소드를 가지고 있는 클래스 생성

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,11 @@
+public class StringUtils {
+
+    public static final String NEWLINE = System.getProperty("line.separator");
+
+    public static String appendNewLine(String original) {
+        return original + NEWLINE;
+    }
+
+    private StringUtils() {
+    }
+}
