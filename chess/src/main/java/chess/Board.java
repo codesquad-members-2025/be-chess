8aabaca23de531019698e74a37e096cde59a7b29
jkchoi490 <jkchoi490@daum.net>
2025-03-06 11:51:06 +0900

    feat : 패키지 분리, 추가 테스트, 중복 제거 기능 개발

---
+++ chess/src/main/java/chess/Board.java
@@ -0,0 +1,29 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class Board {
+
+    ArrayList<Pawn> boardList = new ArrayList<>();
+
+    public void add(Pawn pawn) {
+        boardList.add(pawn);
+    }
+
+    public int size() {
+        return boardList.size();
+    }
+
+    // 인덱스를 사용하여 Pawn 객체 찾기
+    public Pawn findPawn(int idx) {
+        if (isValidIndex(idx)) {
+            return boardList.get(idx);
+        }
+        return null;
+    }
+
+    //유효성 검사
+    private boolean isValidIndex(int idx) {
+        return idx >= 0 && idx < boardList.size();
+    }
+}
---
+++ chess/src/test/java/pieces/BoardTest.java
@@ -0,0 +1,46 @@
+package pieces;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import chess.Board;
+import chess.Pawn;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class BoardTest {
+
+    private Board board;
+    private Pawn white;
+    private Pawn black;
+
+    @Before
+    public void setUp(){
+        board = new Board();
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+    }
+
+    @Test
+    public void create() throws Exception {
+       // Board board = new Board();
+
+        //Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        board.add(white);
+        assertEquals(1, board.size());
+        assertEquals(white, board.findPawn(0));
+
+       // Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        board.add(black);
+        assertEquals(2, board.size());
+        assertEquals(black, board.findPawn(1));
+    }
+
+    //추가 테스트 구현
+    @Test
+    public void add_another(){
+        //Board board = new Board();
+       // board.add(new Integer("7")); -> compile 에러 발생
+    }
+}
