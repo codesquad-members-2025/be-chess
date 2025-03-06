b273722ab3dfbed110710f530c74770bbde97abd
jaehoonchoi <eeoos8840@gmail.com>
2025-03-06 17:51:08 +0900

    Test: BoardTest 추가
    체스판 초기화 세팅을 테스트하는 create 메서드 추가

--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -4,42 +4,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static chess.utils.StringUtils.appendNewLine;
 import static org.assertj.core.api.Assertions.*;
 
 public class BoardTest {
-
     private Board board;
 
     @BeforeEach
-    public void setUp() {
+    public void setup() {
         board = new Board();
     }
 
     @Test
-    @DisplayName("흑/백의 값을 잘 가져오는지 테스트")
-    public void initialize() throws Exception {
-        assertThat("pppppppp").isEqualTo(board.getWhitePawnsResult());
-        assertThat("PPPPPPPP").isEqualTo(board.getBlackPawnsResult());
-    }
-
-    @Test
-    @DisplayName("체스판 출력")
-    public void print() throws Exception{
-        String expectResult =
-                """
-                ........
-                PPPPPPPP
-                ........
-                ........
-                ........
-                ........
-                pppppppp
-                ........
-                """;
-        String result = board.print();
-        System.out.println(result);
-        assertThat(result).isEqualTo(expectResult);
-
+    @DisplayName("체스판 세팅 테스트")
+    public void create() throws Exception {
+        board.initialize();
+        assertThat(board.pieceCount()).isEqualTo(32);
+        String blankRank = appendNewLine("........");
+        assertThat(board.showBoard()).isEqualTo(appendNewLine("RNBQKBNR") +
+                appendNewLine("PPPPPPPP") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("pppppppp") +
+                appendNewLine("rnbqkbnr"));
     }
 }
-
