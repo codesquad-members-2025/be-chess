24971ab313e99c8ec62b5c12ec4cac30872e9d77
2jiyong <jdragon2000@naver.com>
2025-03-06 11:37:56 +0900

    Piece에 모든 기물 팩토리 메서드 추가 PieceTest에서 다른 기물들 확인하는 코드 추가하고, Piece에 모든 기물에 대한 팩토리 메서드 추가

--- src/main/java/org/pieces/Piece.java
+++ src/main/java/org/pieces/Piece.java
@@ -5,32 +5,81 @@ public class Piece {
     public static final String BLACK_COLOR = "black";
     public static final char WHITE_PAWN_REPRESENTATION = 'p';
     public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
 
 
     private final String color;
     private final String name;
     private final char representation;
 
-    private Piece(String color,String name){
+    private Piece(String color, String name, char representation) {
         this.color = color;
         this.name = name;
-        if(color.equals(WHITE_COLOR)) representation = WHITE_PAWN_REPRESENTATION;
-        else representation = BLACK_PAWN_REPRESENTATION;
+        this.representation = representation;
     }
 
-    public String getColor(){
+    public String getColor() {
         return color;
     }
 
-    public char getRepresentation(){
+    public char getRepresentation() {
         return representation;
     }
 
-    public static Piece createWhitePawn(){
-        return new Piece(Piece.WHITE_COLOR,"pawn");
+    public static Piece createWhitePawn() {
+        return new Piece(Piece.WHITE_COLOR, "pawn", Piece.WHITE_PAWN_REPRESENTATION);
     }
 
-    public static Piece createBlackPawn(){
-        return new Piece(Piece.BLACK_COLOR,"pawn");
+    public static Piece createBlackPawn() {
+        return new Piece(Piece.BLACK_COLOR, "pawn", Piece.BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Piece.WHITE_COLOR, "rook", Piece.WHITE_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Piece.BLACK_COLOR, "rook", Piece.BLACK_ROOK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Piece.WHITE_COLOR, "knight", Piece.WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(Piece.BLACK_COLOR, "knight", Piece.BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Piece.WHITE_COLOR, "bishop", Piece.WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Piece.BLACK_COLOR, "bishop", Piece.BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Piece.WHITE_COLOR, "queen", Piece.WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Piece.BLACK_COLOR, "queen", Piece.BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Piece.WHITE_COLOR, "king", Piece.WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Piece.BLACK_COLOR, "king", Piece.BLACK_KING_REPRESENTATION);
     }
 }
--- src/test/java/org/pieces/PieceTest.java
+++ src/test/java/org/pieces/PieceTest.java
@@ -9,6 +9,8 @@ public class PieceTest {
     public void create_piece() {
         verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
         verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
     }
 
     private void verifyPiece(final Piece piece, final String color, final char representation) {
