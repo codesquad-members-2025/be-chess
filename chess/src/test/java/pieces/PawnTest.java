5f10d502fc61b436a35721849c91447309d85c2f
jkchoi490 <jkchoi490@daum.net>
2025-03-06 15:24:48 +0900

    feat: Pawn 색에 따른 출력 문자 부여, 테스트 코드 작성

--- chess/src/test/java/pieces/PawnTest.java
+++ chess/src/test/java/pieces/PawnTest.java
@@ -12,44 +12,22 @@ import static org.assertj.core.api.Assertions.*;
 
 
 public class PawnTest {
-
-    private Pawn whitePawn;
-    private Pawn blackPawn;
-
-    @Before
-    public void setUp(){
-        whitePawn = new Pawn(Pawn.WHITE_COLOR);
-        blackPawn = new Pawn(Pawn.BLACK_COLOR);
-
-    }
-    @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() { // create() 테스트 메소드 작성
-      //  String white = "white"; //local variable을 추가
-       // String black = "black";
-
-      //  Pawn pawn = new Pawn(white);
-       // assertThat(pawn.getColor()).isEqualTo(white);
-
-        //2. Pawn 클래스 구현
-       // Pawn blackPawn = new Pawn(black);
-       // assertThat(blackPawn.getColor()).isEqualTo(black);
-
-
-        assertThat(whitePawn.getColor()).isEqualTo("white");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
-    }
-
-    //중복 코드 제거를 위한 verifyPawn(final String color)
-    public void verifyPawn(final String color){
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
-    }
-
     @Test
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals("white", pawn.getColor());
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 
+    @Test
+    public void create() {
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+    }
+
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
+    }
 }
