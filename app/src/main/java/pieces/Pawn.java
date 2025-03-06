b552a993762d55db02509bdf5a260bea91004c07
jang-jinuk <dlsndifn98@gmail.com>
2025-03-06 10:53:43 +0900

    폰 기호 상수 선언
    - 각 색상마다 폰의 기호를 상수로 선언했습니다.

--- app/src/main/java/pieces/Pawn.java
+++ app/src/main/java/pieces/Pawn.java
@@ -3,6 +3,10 @@ package pieces;
 public class Pawn {
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+
+    public static final char WHITE_REPRESENTATION = '♙';
+    public static final char BLACK_REPRESENTATION = '♟';
+
     private final String color;
 
     public Pawn() {
