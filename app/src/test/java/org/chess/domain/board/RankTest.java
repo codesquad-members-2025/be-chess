af91737ee1a50595a1b41d544fe9b4a875428bf9
gladhee <gladhee@icloud.com>
2025-03-06 16:12:42 +0900

    test(Rank): add test for piece representation by color in Rank

--- app/src/test/java/org/chess/domain/board/RankTest.java
+++ app/src/test/java/org/chess/domain/board/RankTest.java
@@ -27,4 +27,22 @@ class RankTest {
         assertThat(pieces).isEqualTo(rank.getPieces());
     }
 
+    @Test
+    @DisplayName("현재 Rank 에 Pieces 가 색상에 따라 출력되어야 한다")
+    void 색상에_따른_반환_테스트() {
+        // given
+        List<Piece> pieces = List.of(
+                Piece.createWhite(Piece.Type.ROOK), Piece.createBlack(Piece.Type.KNIGHT), Piece.createWhite(Piece.Type.BISHOP),
+                Piece.createWhite(Piece.Type.QUEEN), Piece.createBlack(Piece.Type.KING), Piece.createWhite(Piece.Type.BISHOP),
+                Piece.createWhite(Piece.Type.KNIGHT), Piece.createBlack(Piece.Type.ROOK)
+        );
+        Rank rank = Rank.of(pieces);
+
+        // when
+        String rankString = rank.toString();
+
+        // then
+        assertThat(rankString).isEqualTo("rNbqKbnR");
+    }
+
 }
