cb690de9c6f54c7f974cce8f27b56341eb127caf
moongyuxx <gyuwon822@gmail.com>
2025-03-07 01:22:11 +0900

    feat: 전체 체스판 초기화, 기물 개수 반환 메서드 추가.

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -21,6 +21,22 @@ public class Board {
             board[1][i] = Piece.BLACK_PAWN_REPRESENTATION;
             board[6][i] = Piece.WHITE_PAWN_REPRESENTATION;
         }
+        board[0][0] = Piece.createBlackRook().getRepresentation();
+        board[7][0] = Piece.createWhiteRook().getRepresentation();
+        board[0][1] = Piece.createBlackKnight().getRepresentation();
+        board[7][1] = Piece.createWhiteKnight().getRepresentation();
+        board[0][2] = Piece.createBlackBishop().getRepresentation();
+        board[7][2] = Piece.createWhiteBishop().getRepresentation();
+        board[0][3] = Piece.createBlackQueen().getRepresentation();
+        board[7][3] = Piece.createWhiteQueen().getRepresentation();
+        board[0][4] = Piece.createBlackKing().getRepresentation();
+        board[7][4] = Piece.createWhiteKing().getRepresentation();
+        board[0][5] = Piece.createBlackBishop().getRepresentation();
+        board[7][5] = Piece.createWhiteBishop().getRepresentation();
+        board[0][6] = Piece.createBlackKnight().getRepresentation();
+        board[7][6] = Piece.createWhiteKnight().getRepresentation();
+        board[0][7] = Piece.createBlackRook().getRepresentation();
+        board[7][7] = Piece.createWhiteRook().getRepresentation();
     }
 
     public String getWhitePawnsResult() {
@@ -40,5 +56,17 @@ public class Board {
             sb.append(appendNewLine(""));
         }
         return sb.toString();
+    }
+
+    public int pieceCount() {
+        int cnt = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] != '.') {
+                    cnt ++;
+                }
+            }
+        }
+        return cnt;
     }
 }
