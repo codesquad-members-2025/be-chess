492a13a87f1b5e2383d65a758d73ec3483077f7c
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-03-06 15:18:54 +0900

    [케이딘] step2 3단계: 체스판 초기화 (#40)
    * feat: 프로젝트 기본 구조 생성
    
    * feat: Pawn 클래스 구현
    
    * feat: PawnTest 테스트 클래스 구현
    
    * .gitignore 업데이트 및 프로젝트 파일 추가
    
    * refactor: 의존성 추가, 파일 삭제
    
    build.gradle 파일에 assertj-core 의존성 추가, 필요 없는 파일 삭제
    
    * feat: Pawn 클래스에 기본 생성자 추가
    
    * feat: PawnTest 테스트 추가
    
    기본 생성자를 테스트하는 기능 추가
    
    * refactor: 인터페이스 생성
    
    Piece 인터페이스 생성, Pawn과 다른 말들이 Piece를 구현하도록 함
    
    * feat: Board 클래스 생성
    
    게임판을 구성하는 Board 클래스 생성
    게임판에 들어간 말들을 List로 관리
    
    * refactor: PawnTest 수정
    
    import문 수정
    
    * feat: BoardTest 클래스 구현
    
    체스 게임판을 관리하는 Board 클래스 테스트 작성
    
    * refactor: 파일 재배치
    
    pieces 패키지 생성
    
    * refactor: 중복 제거
    
    JUnit5 의 @BeforeEach 를 사용해서 중복 제거
    
    * refactor: 작은 수정
    
    필요 없는 라인 제거
    
    * feat: Application 클래스 생성
    
    프로그램의 진입점인 main()과 사용자 입력을 관리할 수 있는 클래스 생성
    
    * refactor: PawnTest 수정
    
    루카스 코드와 2단계 코맨트 참고해서 수정
    
    * refactor: Pawn 클래스 수정
    
    Pawn 객체가 색깔에 따른 char 값을 갖도록 함
    
    * refactor: Board 클래스 수정
    
    초기화, 프린트 등등의 기능을 구현
    
    * fix: Application 수정
    
    사용자가 start 입력 시 호출하는 메서드 수정
    
    * refactor: Board 수정
    
    Board 에 새로운 기능 구현
    
    * refactor: BoardTest 수정
    
    BoardTest 테스트 내용 수정

--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -2,30 +2,26 @@ package pieces;
 
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
-    private final String COLOR_W = "white";
-    private final String COLOR_B = "black";
-
-    @Test
-    @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
-    public void create() {
-        verifyPawn(COLOR_W);
-        verifyPawn(COLOR_B);
-    }
-
-    public void verifyPawn(String color) {
-        Pawn whitePawn = new Pawn(color);
-        assertThat(whitePawn.getColor()).isEqualTo(color);
-    }
-
     @Test
     public void createDefaultConstructor() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals(COLOR_W, pawn.getColor());
+        assertEquals(Pawn.COLOR_WHITE, pawn.getColor());
+        assertEquals(Pawn.REPRESENTATION_WHITE, pawn.getRepresentation());
     }
 
+    @Test
+    public void create() {
+        verifyPawn(Pawn.COLOR_WHITE, Pawn.REPRESENTATION_WHITE);
+        verifyPawn(Pawn.COLOR_BLACK, Pawn.REPRESENTATION_BLACK);
+    }
+
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
+    }
 }
