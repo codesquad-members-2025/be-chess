1a1033cd76688b88b600e6eff1bf32d6f77bf4be
Gyuwon Moon <112413876+moongyuxx@users.noreply.github.com>
2025-03-07 08:03:52 +0900

    [BRIE] step2-3 체스판 초기화 (#43)
    * feat: Pawn 색에 따른 출력 문자 부여
    
    * feat: 체스 판 초기화
    * getPawnResult() 메소드로 결과 검증
    
    * feat: print() 구현
    
    * feat: main() 구현
    * start 입력받으면 게임 시작, 현재 상태 출력
    * end 입력받으면 게임 종료

--- app/src/main/java/pieces/Pawn.java
+++ app/src/main/java/pieces/Pawn.java
@@ -2,21 +2,30 @@ package pieces;
 
 
 public class Pawn {
-    public static final String WHITE = "white";
-    public static final String BLACK = "black";
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
+    public static char WHITE_REPRESENTATION = 'p';
+    public static char BLACK_REPRESENTATION = 'P';
 
     private final String color;
+    private final char representation;
 
     // 기본 생성자
     public Pawn() {
-        this.color = WHITE;
+        this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 }
--- app/src/test/java/pieces/PawnTest.java
+++ app/src/test/java/pieces/PawnTest.java
@@ -1,29 +1,26 @@
 package pieces;
 
 import chess.Board;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
-    private Pawn pawn;
-
-    @BeforeEach
-    public void setUp() {
-        pawn = new Pawn(Pawn.BLACK);
+    @Test
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
-    public void create() throws Exception {
-        Pawn pawn = new Pawn();
-        Assertions.assertEquals("white", pawn.getColor());
+    public void create() {
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    public void verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
 }
