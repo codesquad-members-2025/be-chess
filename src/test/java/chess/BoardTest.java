14edd25e8b5026f901a7b7a88ee7d574ae7f44cf
jangsubin <eltm521@naver.com>
2025-03-06 15:42:13 +0900

    test : 기물과 색에 해당하는 기물 갯수 테스트 - 입력 받은 문자열을 보드에 추가 - 해당 보드에서 색과 기물에 해당하는 갯수 검증

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -3,7 +3,11 @@ package chess;
 import static chess.utils.StringUtils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import chess.pieces.Piece;
 import chess.pieces.Piece.Color;
+import chess.pieces.Piece.Type;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,12 +34,41 @@ class BoardTest {
 
     @Test
     @DisplayName("보드초기화시 같은 색의 폰들이 한줄에 있어야한다.")
-    public void initialize() throws Exception {
-        Board board = new Board();
+    void initialize() throws Exception {
         board.initialize();
         assertThat(board.getPawnResult(Color.WHITE)).isEqualTo("♙♙♙♙♙♙♙♙");
         assertThat(board.getPawnResult(Color.BLACK)).isEqualTo("♟♟♟♟♟♟♟♟");
     }
 
+    @Test
+    @DisplayName("색과 종류에 따른 보드 위에 존재하는 기물 갯수를 리턴한다.")
+    void pieceCountOnBoard() {
+        initializeBoardByString();
+        int pawnCount = board.pieceCountOnBoard(Color.BLACK, Type.PAWN);
+        int knightCount = board.pieceCountOnBoard(Color.WHITE, Type.KNIGHT);
 
+        assertThat(pawnCount).isEqualTo(3);
+        assertThat(knightCount).isEqualTo(1);
+    }
+
+    private void initializeBoardByString() {
+        String boardText = ".♚♜.....\n"
+                + "♟.♟♝....\n"
+                + ".♟..♛...\n"
+                + "........\n"
+                + ".....♘♕.\n"
+                + ".....♙..\n"
+                + "......♙.\n"
+                + "....♖♔..";
+
+        String[] split = boardText.split("\n");
+        for (String s : split) {
+            List<Piece> rank = new ArrayList<>();
+            for (int i = 0; i < s.length(); i++) {
+                Piece piece = Piece.createPieceByRepresentation(s.charAt(i));
+                rank.add(piece);
+            }
+            board.addPiece(new Rank(rank));
+        }
+    }
 }
