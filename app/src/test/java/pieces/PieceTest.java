4104a4447e6edebdce93dd22cf7994670dfcfc51
jang-jinuk <dlsndifn98@gmail.com>
2025-03-07 11:45:46 +0900

    기본 생성자 테스트 코드 제거
    Piece의 기본 생성자를 제거하여 관련 테스트 코드를 제거하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -13,14 +13,6 @@ public class PieceTest {
         verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
-    @Test
-    @DisplayName("색상을 지정하지 않으면 흰색 폰이 생성되어야 한다")
-    public void create_default_constructor() {
-        Piece piece = new Piece();
-        assertEquals(Piece.WHITE_COLOR, piece.getColor());
-        assertEquals(Piece.WHITE_PAWN_REPRESENTATION, piece.getRepresentation());
-    }
-
     void verifyPawn(final String color, final char representation) {
         Piece piece = new Piece(color, representation);
         assertEquals(color, piece.getColor());
