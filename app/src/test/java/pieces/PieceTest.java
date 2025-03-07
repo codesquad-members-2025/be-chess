8db62b0b00819aa20ff229b94e3d63546be5b36d
jang-jinuk <dlsndifn98@gmail.com>
2025-03-07 12:02:43 +0900

    퀸 생성 테스트 코드 작성
    퀸이 정상적으로 생성되는지 확인하는 테스트 코드를 작성하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -34,6 +34,13 @@ public class PieceTest {
         verifyPawn(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
+    @Test
+    @DisplayName("흰색, 검은색 퀸이 생성되어야 한다")
+    public void create_piece() {
+        verifyPawn(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPawn(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+    }
+
     private void verifyPawn(final Piece piece, final String color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
