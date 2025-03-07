3c1ea25a28e29165062f1208707bc2a681942930
jang-jinuk <dlsndifn98@gmail.com>
2025-03-07 11:49:42 +0900

    create 테스트 코드 수정
    - 팩토리 메서드 생성을 통해 Piece 인스턴스를 생성하도록 테스트 코드 변경하였습니다.
    - create -> create_piece 메서드 명칭 변경하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -8,13 +8,12 @@ public class PieceTest {
 
     @Test
     @DisplayName("흰색 또는 검은색 폰이 생성되어야 한다")
-    public void create() {
-        verifyPawn(Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
-        verifyPawn(Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+    public void create_piece() {
+        verifyPawn(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPawn(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
-    void verifyPawn(final String color, final char representation) {
-        Piece piece = new Piece(color, representation);
+    void verifyPawn(final Piece piece, final String color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
     }
