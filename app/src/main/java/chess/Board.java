9155034c38a5e2ed5ceee53387bf7884b55150bc
Gyuwon Moon <112413876+moongyuxx@users.noreply.github.com>
2025-03-07 11:44:11 +0900

    [BRIE] step2-4 모든 기물 배치하기  (#50)
    * feat: Pawn 색에 따른 출력 문자 부여
    
    * feat: 체스 판 초기화
    * getPawnResult() 메소드로 결과 검증
    
    * feat: print() 구현
    
    * feat: main() 구현
    * start 입력받으면 게임 시작, 현재 상태 출력
    * end 입력받으면 게임 종료
    
    * feat: "\n"의 반복 코드 제거
    * System.getProperty("line.separator")으로 구현
    
    * refactor: Pawn 클래스 이름을 Piece로 rename
    * Piece는 color와 representation을 가지도록 변경
    * private 사용, Piece는 값 객체
    * 기본 생성자 제거, 팩토리 메서드로 객체 생성하도록 수정
    
    * feat: 전체 체스판 초기화, 기물 개수 반환 메서드 추가.
    
    * feat: 검은색, 흰색 말 구분 메소드 추가
    
    * refactor: merge 충돌 발생 -> 삭제

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -1,9 +1,9 @@
 package chess;
 
-import pieces.Pawn;
+import pieces.Piece;
 
-import java.util.ArrayList;
-import java.util.List;
+import static utils.StringUtils.appendNewLine;
+
 
 public class Board {
     private final char[][] board =  new char[8][8];
@@ -18,9 +18,25 @@ public class Board {
 
     public void initialize() {
         for (int i = 0; i < 8; i++) {
-            board[1][i] = Pawn.BLACK_REPRESENTATION;
-            board[6][i] = Pawn.WHITE_REPRESENTATION;
+            board[1][i] = Piece.BLACK_PAWN_REPRESENTATION;
+            board[6][i] = Piece.WHITE_PAWN_REPRESENTATION;
         }
+        board[0][0] = Piece.createBlackRook().getRepresentation();
+        board[7][0] = Piece.createWhiteRook().getRepresentation();
+        board[0][1] = Piece.createBlackKnight().getRepresentation();
+        board[7][1] = Piece.createWhiteKnight().getRepresentation();
+        board[0][2] = Piece.createBlackBishop().getRepresentation();
+        board[7][2] = Piece.createWhiteBishop().getRepresentation();
+        board[0][3] = Piece.createBlackQueen().getRepresentation();
+        board[7][3] = Piece.createWhiteQueen().getRepresentation();
+        board[0][4] = Piece.createBlackKing().getRepresentation();
+        board[7][4] = Piece.createWhiteKing().getRepresentation();
+        board[0][5] = Piece.createBlackBishop().getRepresentation();
+        board[7][5] = Piece.createWhiteBishop().getRepresentation();
+        board[0][6] = Piece.createBlackKnight().getRepresentation();
+        board[7][6] = Piece.createWhiteKnight().getRepresentation();
+        board[0][7] = Piece.createBlackRook().getRepresentation();
+        board[7][7] = Piece.createWhiteRook().getRepresentation();
     }
 
     public String getWhitePawnsResult() {
@@ -37,8 +53,20 @@ public class Board {
             for (int j = 0; j < 8; j++) {
                 sb.append(board[i][j]);
             }
-            sb.append("\n");
+            sb.append(appendNewLine(""));
         }
         return sb.toString();
+    }
+
+    public int pieceCount() {
+        int cnt = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] != '.') {
+                    cnt ++;
+                }
+            }
+        }
+        return cnt;
     }
 }
