058022d3831601597b305c7efe78fd263adf8374
2jiyong <jdragon2000@naver.com>
2025-03-07 12:18:15 +0900

    GameController 간단한 코드 순서 변경

--- src/main/java/org/chess/GameController.java
+++ src/main/java/org/chess/GameController.java
@@ -11,19 +11,19 @@ public class GameController {
         this.board = board;
     }
 
-    public Piece findPiece(String location) {
-        Coordinate coordinate = parseCoordinate(location);
-        int fileIndex = coordinate.getFileIndex();
-        int rankIndex = coordinate.getRankIndex();
-        return rankList().get(rankIndex).getPieceByFileIndex(fileIndex);
-    }
-
     public void move(String sourcePosition, String targetPosition) {
         Piece source = findPiece(sourcePosition);
         // target에 기물 추가
         putPiece(targetPosition, source);
         // 원래 자리에 blank 추가
         putPiece(sourcePosition, Piece.createBlank());
+    }
+
+    public Piece findPiece(String location) {
+        Coordinate coordinate = parseCoordinate(location);
+        int fileIndex = coordinate.getFileIndex();
+        int rankIndex = coordinate.getRankIndex();
+        return rankList().get(rankIndex).getPieceByFileIndex(fileIndex);
     }
 
     public void putPiece(String location, Piece piece) {
