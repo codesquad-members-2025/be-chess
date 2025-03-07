6834147115904736bc16388e861fe7455527849e
jang-jinuk <dlsndifn98@gmail.com>
2025-03-07 14:17:40 +0900

    BoardTest 수정
    - initialize,  verifyBoard 메서드 제거하였습니다.
    - create 테스트 메서드 변경하였습니다.
    - board를 초기화하고 32가지의 기물이 정상적으로 배치되었는지 검증합니다.

--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -1,48 +1,31 @@
 package chess;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pieces.Piece;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static utils.StringUtils.appendNewLine;
 
 public class BoardTest {
-    private Piece[] pieces;
     private Board board;
 
     @BeforeEach
     void setUp() {
-        String[] colors = {Piece.WHITE_COLOR, Piece.BLACK_COLOR};
-        char[] representation = {Piece.WHITE_PAWN_REPRESENTATION, Piece.BLACK_PAWN_REPRESENTATION};
-        pieces = new Piece[colors.length];
-
-        for (int i = 0; i < pieces.length; ++i) {
-            pieces[i] = new Piece(colors[i], representation[i]);
-        }
+        board = new Board();
     }
 
     @Test
-    @DisplayName("체스판에 흰색, 검은색 폰이 각각 8개가 추가되어야한다.")
-    public void initialize() throws Exception{
-        board = new Board();
+    public void create() throws Exception{
         board.initialize();
-        assertEquals("♙♙♙♙♙♙♙♙", board.getWhitePawnResult());
-        assertEquals("♟♟♟♟♟♟♟♟", board.getBlackPawnResult());
-    }
+        assertEquals(32, board.pieceCount());
+        String blankRank = appendNewLine("........");
+        assertEquals(
+            appendNewLine("♜♞♝♛♚♝♞♜") +
+                appendNewLine("♟♟♟♟♟♟♟♟") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("♙♙♙♙♙♙♙♙") +
+                appendNewLine("♖♘♗♕♔♗♘♖"),
+                board.showBoard());
 
-    @Test
-    public void create() {
-        board = new Board();
-        for (int i = 0; i < pieces.length; ++i) {
-            board.add(pieces[i]);
-            verifyBoard(i);
-        }
-    }
-
-    // Pawn이 Board에 정상적으로 추가되는지 확인한다.
-    void verifyBoard(int i) {
-        assertEquals(i + 1, board.size());
-        assertEquals(pieces[i], board.findPawn(i));
     }
 }
