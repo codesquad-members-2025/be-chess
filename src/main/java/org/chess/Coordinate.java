d9dae6a90949ce6b151c4b4bdfaa1eeb9e6d1951
2jiyong <jdragon2000@naver.com>
2025-03-07 13:49:19 +0900

    리팩토링 : Coordinate 생성자 매개변수 변경함 int 두 개를 매개변수로 받는 생성자 추가로 만들어주고, 기존에 있던 String을 받는 생성자는 자신의 생성자를 호출하도록 함.

--- src/main/java/org/chess/Coordinate.java
+++ src/main/java/org/chess/Coordinate.java
@@ -1,28 +1,32 @@
 package org.chess;
 
 public class Coordinate {
-    private final int rankIndex;
     private final int fileIndex;
+    private final int rankIndex;
 
-    public Coordinate(String location){
-        if(!isValid(location)) throw new IllegalArgumentException("잘못된 좌표입니다.");
-        rankIndex = Character.getNumericValue(location.charAt(1))-1;
-        fileIndex = location.charAt(0)-'a';
+    public Coordinate(int fileIndex, int rankIndex) {
+        if (!isValid(fileIndex, rankIndex)) throw new IllegalArgumentException("잘못된 좌표입니다.");
+        this.fileIndex = fileIndex;
+        this.rankIndex = rankIndex;
     }
 
-    private boolean isValid(String location){
-        char file =location.charAt(0);
-        char rank =location.charAt(1);
-        if(!(file>='a' && file<='h')) return false;
-        if(!(rank>='1' && rank<='8')) return false;
-        return true;
+    public Coordinate(String location) {
+        this(location.charAt(0) - 'a', Character.getNumericValue(location.charAt(1)) - 1);
     }
 
-    int getRankIndex(){
+    private boolean isValid(int fileIndex, int rankIndex) {
+        return fileIndex >= 0 && fileIndex < 8 && rankIndex >= 0 && rankIndex < 8;
+    }
+
+    public Coordinate moveCoordinate(int fileDegree, int rankDegree) {
+        return new Coordinate(fileIndex + fileDegree, rankIndex + rankDegree);
+    }
+
+    int getRankIndex() {
         return rankIndex;
     }
 
-    int getFileIndex(){
+    int getFileIndex() {
         return fileIndex;
     }
 
