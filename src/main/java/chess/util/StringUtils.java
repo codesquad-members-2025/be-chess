6aa7db40f33ed03e761ccd1b9c1929cce76adf3f
johnnie <come753zz@gmail.com>
2025-03-07 09:37:32 +0900

    5단계: 기물 위치 부여 및 점수계산

--- src/main/java/chess/util/StringUtils.java
+++ src/main/java/chess/util/StringUtils.java
@@ -2,7 +2,7 @@ package chess.util;
 
 public class StringUtils {
 
-    public static final String NEWLINE = System.getProperty("line.separator");
+    public static final String NEWLINE = System.lineSeparator();
 
     private StringUtils() {
         // 유틸 클래스는 생성자 호출 방지
