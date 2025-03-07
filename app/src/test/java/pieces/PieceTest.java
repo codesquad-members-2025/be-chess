15fa3307be2d17da89a34bdc1b4c251b1e1edd4f
jang-jinuk <dlsndifn98@gmail.com>
2025-03-07 11:55:59 +0900

    룩 생성 테스트 코드 작성
    룩이 정상적으로 생성되는지 확인하는 테스트 코드를 작성하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -13,6 +13,13 @@ public class PieceTest {
         verifyPawn(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
     }
 
+    @Test
+    @DisplayName("흰색, 검은색 룩이 생성되어야 한다")
+    public void create_piece() {
+        verifyPawn(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPawn(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+    }
+
     private void verifyPawn(final Piece piece, final String color, final char representation) {
         assertEquals(color, piece.getColor());
         assertEquals(representation, piece.getRepresentation());
