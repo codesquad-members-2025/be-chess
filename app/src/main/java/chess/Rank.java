a8e5885e47872936974c1b73bf98bc709cfdea00
jaehoonchoi <eeoos8840@gmail.com>
2025-03-08 22:29:45 +0900

    Feat(Rank): Piece를 반환하는 getPiece 생성
    - getPiece(int file): rank에서 인자의 file에 해당하는 Piece를 반환하는 메서드 추가

--- app/src/main/java/chess/Rank.java
+++ app/src/main/java/chess/Rank.java
@@ -1,2 +1,35 @@
-package chess;public class Rank {
+package chess;
+
+
+import chess.pieces.Piece;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static chess.Board.RANK_SIZE;
+
+public class Rank {
+    private final List<Piece> rank;
+
+    public Rank(List<Piece> rank) {
+        this.rank = rank;
+        validateRankSize(rank.size());
+    }
+
+    public Piece getPiece(int file) {
+        return rank.get(file);
+    }
+
+    private void validateRankSize(int size) {
+        if (size > RANK_SIZE) {
+            throw new IllegalStateException(RANK_SIZE + "를 초과할 수 없습니다. 현재 사이즈: " + size);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return rank.stream()
+                .map(i -> String.valueOf(i.getRepresentation()))
+                .collect(Collectors.joining());
+    }
 }
