0f723b94cf0f70dc018865bb8c55c291ba362708
jaehoonchoi <eeoos8840@gmail.com>
2025-03-09 16:45:25 +0900

    Feat(Rank): 한 랭크의 원하는 위치에 기물을 삽입하는 메서드 생성
    - 임시 리스트를 생성하여 원하는 위치에 기물을 삽입한 뒤 반환하는 updatePiece 메서드 생성

--- app/src/main/java/chess/Rank.java
+++ app/src/main/java/chess/Rank.java
@@ -3,6 +3,7 @@ package chess;
 
 import chess.pieces.Piece;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,15 @@ public class Rank {
         return rank.get(file);
     }
 
+    public Rank updatePiece(int file, Piece piece) {
+        ArrayList<Piece> tmpRank = new ArrayList<>(this.rank);
+        tmpRank.set(file, piece);
+        return new Rank(tmpRank);
+
+        // 아래와 같이 하면 file에 해당하는 모든 기물 변경됨
+        // 위처럼 복사를 한 뒤 변경하고 반환해야함
+//        rank.set(file, piece);
+    }
     private void validateRankSize(int size) {
         if (size > RANK_SIZE) {
             throw new IllegalStateException(RANK_SIZE + "를 초과할 수 없습니다. 현재 사이즈: " + size);
