020ada8e013dac902c1884484582c21bf7e544bc
jaehoonchoi <eeoos8840@gmail.com>
2025-03-09 19:16:58 +0900

    Fix(Position): 좌표값 검증 버그 수정
    - x 좌표 유효값을 1~8로 되어 있던 것을 0~7로 수정

--- app/src/main/java/chess/Position.java
+++ app/src/main/java/chess/Position.java
@@ -27,8 +27,8 @@ public class Position {
     }
 
     private void validatePositionRange(int[] positionArr) {
-        if (1 > positionArr[0] || positionArr[0] > 8) {
-            throw new IllegalStateException("x 좌표값이 유효하지 않습니다. 1~8 사이의 좌표를 입력해주세요. 현재 입력 좌표값: " + positionArr[0]);
+        if (0 > positionArr[0] || positionArr[0] > 7) {
+            throw new IllegalStateException("x 좌표값이 유효하지 않습니다. 1~8 사이의 좌표를 입력해주세요. 현재 입력 좌표값: " + positionArr[0]+1);
         }
         if (0 > positionArr[1] || positionArr[1] > 7) {
             throw new IllegalStateException("y 좌표값이 유효하지 않습니다. 1~8 사이의 좌표를 입력해주세요. 현재 입력 좌표값: " + positionArr[1]+1);
