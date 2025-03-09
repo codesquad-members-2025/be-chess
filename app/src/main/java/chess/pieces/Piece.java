55b67f055c55b32381e41cdcdb8d6071e77aae9f
jaehoonchoi <eeoos8840@gmail.com>
2025-03-09 18:25:56 +0900

    Feat(Piece): Piece의 Type enum에 점수 필드 추가
    - 점수 getter 추가

--- app/src/main/java/chess/pieces/Piece.java
+++ app/src/main/java/chess/pieces/Piece.java
@@ -83,12 +83,14 @@ public class Piece {
     }
 
     public enum Type {
-        PAWN('p'), ROOK('r'), KNIGHT('n'), BISHOP('b'), QUEEN('q'), KING('k'), NO_PIECE('.');
+        PAWN('p', 1.0), ROOK('r', 5.0), KNIGHT('n', 2.5), BISHOP('b', 3.0), QUEEN('q', 9.0), KING('k', 0.0), NO_PIECE('.', 0.0);
 
         private final char representation;
+        private final double defaultPoint;
 
-        Type(char representation) {
+        Type(char representation, double defaultPoint) {
             this.representation = representation;
+            this.defaultPoint = defaultPoint;
         }
 
         public char getWhiteRepresentation() {
@@ -96,6 +98,9 @@ public class Piece {
         }
         public char getBlackRepresentation() {
             return Character.toUpperCase(representation);
+        }
+        public double getDefaultPoint() {
+            return defaultPoint;
         }
     }
     @Override
