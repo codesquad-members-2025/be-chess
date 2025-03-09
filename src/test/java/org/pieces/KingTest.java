4b5e87ca78e1240ba7a1623e6b23662addf1bb27
2jiyong <jdragon2000@naver.com>
2025-03-09 20:04:23 +0900

    Refactor(KingTest): 테스트코드 리팩토링

--- src/test/java/org/pieces/KingTest.java
+++ src/test/java/org/pieces/KingTest.java
@@ -4,12 +4,16 @@ import org.chess.Board;
 import org.chess.Coordinate;
 import org.chess.Game;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class KingTest {
     Board board;
     Game game;
     Piece king;
+    Coordinate source;
 
     @BeforeEach
     void setUp() {
@@ -17,55 +21,44 @@ public class KingTest {
         game = new Game(board);
         board.initializeEmpty();
         king = Piece.createWhiteKing();
+        source = new Coordinate("e4");
+        board.putPiece(source, king);
     }
 
-    @Test
+    @ParameterizedTest(name = "King move from e4 to {0}")
+    @ValueSource(strings = {"e5", "f4", "f5", "d3", "d4", "d5", "f3", "e3"})
     @DisplayName("왕이 모든 방향으로 이동할 수 있는지 확인")
-    public void move_direction() throws Exception {
-        board.putPiece(new Coordinate("e4"), king);
-        Coordinate position1 = new Coordinate("e5");
-        Coordinate position2 = new Coordinate("f4");
-        Coordinate position3 = new Coordinate("f5");
-        Coordinate position4 = new Coordinate("d3");
-        Coordinate position5 = new Coordinate("d4");
-        Coordinate position6 = new Coordinate("d5");
-        Coordinate position7 = new Coordinate("f3");
-        Coordinate position8 = new Coordinate("e3");
-
-        Coordinate position9 = new Coordinate("e8");
-
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position1)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position2)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position3)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position4)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position5)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position6)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position7)).isTrue();
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position8)).isTrue();
-
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), position9)).isFalse();
+    public void move_direction(String targetCoordinate) throws Exception {
+        Coordinate target = new Coordinate(targetCoordinate);
+        assertThat(king.verifyMovePosition(board, source, target)).isTrue();
     }
 
+    @ParameterizedTest(name = "King can't move from e4 to {0}")
+    @ValueSource(strings = {"e1", "e2","e6", "e7", "e8", "f1", "f2", "d6", "d7", "d8"})
+    @DisplayName("왕이 이동 할 수 없는 곳 이동할 수 없는지 확인")
+    public void move_to_invalid(String targetCoordinate) throws Exception {
+        Coordinate target = new Coordinate(targetCoordinate);
+        assertThat(king.verifyMovePosition(board,source,target)).isFalse();
+    }
+    
     @Test
     @DisplayName("왕이 같은 색으로 이동 할 수 없는지 확인")
     public void move_to_same_team() throws Exception {
-        board.putPiece(new Coordinate("e4"), king);
         board.putPiece(new Coordinate("e5"), Piece.createWhitePawn());
 
         Coordinate targetP = new Coordinate("e5");
 
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), targetP)).isFalse();
+        assertThat(king.verifyMovePosition(board,source, targetP)).isFalse();
     }
 
     @Test
     @DisplayName("왕이 다른 색으로 이동 할 수 있는지 확인")
     public void move_to_enemy() throws Exception {
-        board.putPiece(new Coordinate("e4"), king);
         board.putPiece(new Coordinate("e5"), Piece.createBlackPawn());
 
         Coordinate targetP = new Coordinate("e5");
 
-        assertThat(king.verifyMovePosition(board, new Coordinate("e4"), targetP)).isTrue();
+        assertThat(king.verifyMovePosition(board, source, targetP)).isTrue();
     }
 }
 
