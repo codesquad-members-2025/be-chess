9c35bfd441bb2d6da1d9b4a1b196762d27d9ce1e
2jiyong <jdragon2000@naver.com>
2025-03-09 18:14:00 +0900

    Refactor: 주석과 필요없는 코드 제거

--- src/main/java/org/chess/GameController.java
+++ src/main/java/org/chess/GameController.java
@@ -21,32 +21,13 @@ public class GameController {
             System.out.println("이동 범위가 아닙니다.");
             return;
         }
-        // !!추가해야함
-        // 한칸씩 가면서 빈칸이 아닌 기물을 만나면 무조건 멈춤
-        // 출발지와 도착지 사이에는 모든 칸이 빈칸이어야 함!
-        // 생각해보니 이게 나이트는?
-        // 다시 생각하니 piece에게 board 전달해서 찾는 것으로 구현하는게 나을 듯함.
-
-
-
-
-
-
-
-        // 도착 지점이 같은 색의 기물이라면 이동 불가!
-        if(board.findPiece(targetCoordinate).getColor()==source.getColor()) {
-            System.out.println("도착지점이 같은 색의 기물입니다.");
-            return;
-        }
         // 이동 가능하면 이동!
         // target에 기물 추가
         board.putPiece(targetCoordinate, source);
         // 원래 자리에 blank 추가
         board.putPiece(sourceCoordinate, Piece.createBlank());
     }
-
-
-
+    
     private Coordinate parseCoordinate(String location) {
         try {
             return new Coordinate(location);
