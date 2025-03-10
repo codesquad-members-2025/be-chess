165856be3fb24f6b6bc9b0c12988341248f9cf00
jang-jinuk <dlsndifn98@gmail.com>
2025-03-10 19:32:19 +0900

    create_piece 테스트 메서드 리팩토링
    - 기존 체스 기물 생성 메서드를 createWhite, createBlack 두가지로 통합하여 중복 제거하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -3,6 +3,8 @@ package pieces;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,12 +14,17 @@ public class PieceTest {
     @Test
     @DisplayName("흰색, 검은색 기물이 생성되어야 한다.")
     void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Piece.Type.PAWN);
-        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Piece.Type.ROOK);
-        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Piece.Type.KNIGHT);
-        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Piece.Type.BISHOP);
-        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Piece.Type.QUEEN);
-        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Piece.Type.KING);
+        //Given
+        List<Piece> whitePieces = Piece.createWhite();
+        List<Piece> blackPieces = Piece.createBlack();
+        int piecesIndex = 0;
+
+        //When & Than
+        for (Piece.Type type : Piece.Type.values()) {
+            if (Piece.Type.NO_PIECE == type) continue;
+            verifyPiece(whitePieces.get(piecesIndex), blackPieces.get(piecesIndex), type);
+            ++piecesIndex;
+        }
     }
 
     @Test
