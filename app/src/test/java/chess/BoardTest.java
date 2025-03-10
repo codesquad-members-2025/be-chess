f1128d95abf196598f7d9891d135b77cc8fe42c4
jang-jinuk <dlsndifn98@gmail.com>
2025-03-10 12:41:59 +0900

    create 테스트 메서드 리팩토링
    - Given-When-Then 패턴을 사용하여 가독성을 높였습니다.

--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -3,7 +3,7 @@ package chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static utils.StringUtils.appendNewLine;
 
 public class BoardTest {
@@ -16,16 +16,22 @@ public class BoardTest {
 
     @Test
     public void create() throws Exception{
+        //Given
         board.initialize();
-        assertEquals(32, board.pieceCount());
-        String blankRank = appendNewLine("........");
-        assertEquals(
-            appendNewLine("♜♞♝♛♚♝♞♜") +
-                appendNewLine("♟♟♟♟♟♟♟♟") +
-                blankRank + blankRank + blankRank + blankRank +
-                appendNewLine("♙♙♙♙♙♙♙♙") +
-                appendNewLine("♖♘♗♕♔♗♘♖"),
-                board.showBoard());
 
+        //When
+        int pieceCount = board.pieceCount();
+        String actualBoard = board.showBoard();
+
+        //Then
+        String blankRank = appendNewLine("........");
+        String expectedBoard = appendNewLine("♜♞♝♛♚♝♞♜") +
+            appendNewLine("♟♟♟♟♟♟♟♟") +
+            blankRank + blankRank + blankRank + blankRank +
+            appendNewLine("♙♙♙♙♙♙♙♙") +
+            appendNewLine("♖♘♗♕♔♗♘♖");
+
+        assertThat(pieceCount).isEqualTo(32);
+        assertThat(actualBoard).isEqualTo(expectedBoard);
     }
 }
