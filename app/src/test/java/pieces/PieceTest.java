be2b37317763f3e2bc4f1cba87ee63b323bbed9b
jang-jinuk <dlsndifn98@gmail.com>
2025-03-10 10:29:33 +0900

    기물 생성 테스트 코드 통합
    - 모든 기물 생성 코드를 create 메서드로 통합하였습니다.
    - 가독성을 높이기 위해 assertEquals -> assertThat 메서드로 변경하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -1,55 +1,36 @@
 package pieces;
 
 import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PieceTest {
 
     @Test
-    @DisplayName("흰색, 검은색 폰이 생성되어야 한다")
-    public void create_pawn() {
+    @DisplayName("흰색, 검은색 기물이 생성되어야 한다.")
+    void create() {
         verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
         verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
-    }
 
-    @Test
-    @DisplayName("흰색, 검은색 룩이 생성되어야 한다")
-    public void create_rook() {
         verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
         verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
-    }
 
-    @Test
-    @DisplayName("흰색, 검은색 나이트가 생성되어야 한다")
-    public void create_knight() {
         verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
         verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
-    }
 
-    @Test
-    @DisplayName("흰색, 검은색 비숍이 생성되어야 한다")
-    public void create_bishop() {
         verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
         verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
-    }
 
-    @Test
-    @DisplayName("흰색, 검은색 퀸이 생성되어야 한다")
-    public void create_queen() {
         verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
         verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
-    }
 
-    @Test
-    @DisplayName("흰색, 검은색 킹이 생성되어야 한다")
-    public void create_king() {
         verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
         verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
     private void verifyPiece(final Piece piece, final String color, final char representation) {
-        assertEquals(color, piece.getColor());
-        assertEquals(representation, piece.getRepresentation());
+        assertThat(piece.getColor()).isEqualTo(color);
+        assertThat(piece.getRepresentation()).isEqualTo(representation);
     }
+
+
 }
