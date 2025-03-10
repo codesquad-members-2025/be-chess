02b2263480ad97871b1ec2ac83418ca743e2daf8
jang-jinuk <dlsndifn98@gmail.com>
2025-03-10 17:10:03 +0900

    기물 생성 테스트 코드 리팩토링
    - 색상마다 존재하던 각 기물의 타입을 하나라로 통합해주면서 기물 생성 테스트 코드도 방식을 변경하였습니다.

--- app/src/test/java/pieces/PieceTest.java
+++ app/src/test/java/pieces/PieceTest.java
@@ -1,35 +1,30 @@
 package pieces;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PieceTest {
 
     @Test
     @DisplayName("흰색, 검은색 기물이 생성되어야 한다.")
     void create_piece() {
-        verifyPiece(Piece.createWhitePawn(), Piece.Color.WHITE, Piece.Type.PAWN);
-        verifyPiece(Piece.createBlackPawn(), Piece.Color.BLACK, Piece.Type.PAWN);
-
-        verifyPiece(Piece.createWhiteRook(), Piece.Color.WHITE, Piece.Type.ROOK);
-        verifyPiece(Piece.createBlackRook(), Piece.Color.BLACK, Piece.Type.ROOK);
-
-        verifyPiece(Piece.createWhiteKnight(), Piece.Color.WHITE, Piece.Type.KNIGHT);
-        verifyPiece(Piece.createBlackKnight(), Piece.Color.BLACK, Piece.Type.KNIGHT);
-
-        verifyPiece(Piece.createWhiteBishop(), Piece.Color.WHITE, Piece.Type.BISHOP);
-        verifyPiece(Piece.createBlackBishop(), Piece.Color.BLACK, Piece.Type.BISHOP);
-
-        verifyPiece(Piece.createWhiteQueen(), Piece.Color.WHITE, Piece.Type.QUEEN);
-        verifyPiece(Piece.createBlackQueen(), Piece.Color.BLACK, Piece.Type.QUEEN);
-
-        verifyPiece(Piece.createWhiteKing(), Piece.Color.WHITE, Piece.Type.KING);
-        verifyPiece(Piece.createBlackKing(), Piece.Color.BLACK, Piece.Type.KING);
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Piece.Type.PAWN);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Piece.Type.ROOK);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Piece.Type.KNIGHT);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Piece.Type.BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Piece.Type.QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Piece.Type.KING);
     }
 
-    private void verifyPiece(final Piece piece, final Piece.Color color, final Piece.Type representation) {
-        assertThat(piece.getColor()).isEqualTo(color);
-        assertThat(piece.getType()).isEqualTo(representation);
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Piece.Type type) {
+        assertTrue(whitePiece.isWhite());
+        assertThat(whitePiece.getType()).isEqualTo(type.getRepresentation());
+
+        assertTrue(blackPiece.isBlack());
+        assertThat(blackPiece.getType()).isEqualTo(type.getRepresentation());
     }
 
     @Test
