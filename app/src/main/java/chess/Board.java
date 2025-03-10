4a216aa70e71c5257e1537f2f21552cb443ee34b
moongyuxx <gyuwon822@gmail.com>
2025-03-10 11:17:11 +0900

    체스판의 모든 칸을 Piece로 초기화 * ArrayList<Rank>구조로 변경

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -2,54 +2,53 @@ package chess;
 
 import pieces.Piece;
 import pieces.Piece.Type;
-import pieces.Piece.Color;
-
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import static utils.StringUtils.appendNewLine;
 
-
 public class Board {
-    private final Piece[][] board =  new Piece[8][8];
-    
+    private final ArrayList<Rank> board = new ArrayList<>();
+
     public Board() {
-        for  (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = Piece.createBlank();
-            }
+        for (int i = 0; i < 8; i++) {
+            board.add(new Rank());
         }
     }
 
     public void initialize() {
         for (int i = 0; i < 8; i++) {
-            board[1][i] = Piece.createBlack(Type.PAWN);
-            board[6][i] = Piece.createWhite(Type.PAWN);
+            setPiece(1, i, Piece.createBlack(Type.PAWN));
+            setPiece(6, i, Piece.createWhite(Type.PAWN));
         }
-        board[0][0] = Piece.createBlack(Type.ROOK);
-        board[7][0] = Piece.createWhite(Type.ROOK);
-        board[0][1] = Piece.createBlack(Type.KNIGHT);
-        board[7][1] = Piece.createWhite(Type.KNIGHT);
-        board[0][2] = Piece.createBlack(Type.BISHOP);
-        board[7][2] = Piece.createWhite(Type.BISHOP);
-        board[0][3] = Piece.createBlack(Type.QUEEN);
-        board[7][3] = Piece.createWhite(Type.QUEEN);
-        board[0][4] = Piece.createBlack(Type.KING);
-        board[7][4] = Piece.createWhite(Type.KING);
-        board[0][5] = Piece.createBlack(Type.BISHOP);
-        board[7][5] = Piece.createWhite(Type.BISHOP);
-        board[0][6] = Piece.createBlack(Type.KNIGHT);
-        board[7][6] = Piece.createWhite(Type.KNIGHT);
-        board[0][7] = Piece.createBlack(Type.ROOK);
-        board[7][7] = Piece.createWhite(Type.ROOK);
-        }
+        setPiece(0, 0, Piece.createBlack(Type.ROOK));
+        setPiece(7, 0, Piece.createWhite(Type.ROOK));
+        setPiece(0, 1, Piece.createBlack(Type.KNIGHT));
+        setPiece(7, 1, Piece.createWhite(Type.KNIGHT));
+        setPiece(0, 2, Piece.createBlack(Type.BISHOP));
+        setPiece(7, 2, Piece.createWhite(Type.BISHOP));
+        setPiece(0, 3, Piece.createBlack(Type.QUEEN));
+        setPiece(7, 3, Piece.createWhite(Type.QUEEN));
+        setPiece(0, 4, Piece.createBlack(Type.KING));
+        setPiece(7, 4, Piece.createWhite(Type.KING));
+        setPiece(0, 5, Piece.createBlack(Type.BISHOP));
+        setPiece(7, 5, Piece.createWhite(Type.BISHOP));
+        setPiece(0, 6, Piece.createBlack(Type.KNIGHT));
+        setPiece(7, 6, Piece.createWhite(Type.KNIGHT));
+        setPiece(0, 7, Piece.createBlack(Type.ROOK));
+        setPiece(7, 7, Piece.createWhite(Type.ROOK));
+    }
+
+    public void setPiece(int row, int col, Piece piece) {
+        board.get(row).setPiece(col, piece);  //  Rank의 setPiece() 호출
+    }
+
+
 
 
     public String print() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                sb.append(board[i][j].getRepresentation());
-            }
+            sb.append(board.get(i).toString());
             sb.append(appendNewLine(""));
         }
         return sb.toString();
@@ -57,13 +56,16 @@ public class Board {
 
     public int pieceCount() {
         int cnt = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (board[i][j].getType() == Type.NO_PIECE) {
+        for (Rank row : board) {
+            for (Piece piece : row.getRow()) {
+                if  (piece.getType() != Type.NO_PIECE) {
                     cnt ++;
                 }
             }
+
         }
         return cnt;
     }
 }
+
+
