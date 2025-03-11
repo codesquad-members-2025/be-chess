f2f8afd8568e712d09590b0dcea010a55bd03579
jangsubin <eltm521@naver.com>
2025-03-11 12:45:25 +0900

    refactor : 리터럴 상수화

--- src/main/java/chess/springchess/ChessController.java
+++ src/main/java/chess/springchess/ChessController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChessController {
+    private static final String INIT_BOARD = "보드판이 초기화 되었습니다.";
+    private static final String MOVE_PIECE_SUCCESS = "기물이 정상적으로 이동하였습니다.";
     private final ChessGame chessGame;
     private final Board board;
     private final ChessView chessView;
@@ -23,15 +25,14 @@ public class ChessController {
     @GetMapping(value = "/api/start")
     public Result<ChessDto.initGameDto> startGame() {
         board.initialize();
-        return Result.onSuccess(ChessConverter.createInitGameDto(chessView.showBoard()), "보드판이 초기화 되었습니다.");
+        return Result.onSuccess(ChessConverter.createInitGameDto(chessView.showBoard()), INIT_BOARD);
     }
 
     @PostMapping("api/move")
     public Result<ChessDto.movePieceDto> movePiece(@RequestBody ChessDto.movePieceDto request) {
         try {
             chessGame.move(request.getStartPos(), request.getEndPos());
-            return Result.onSuccess(ChessConverter.createmovePieceDto(request.getStartPos(), request.getEndPos()),
-                    "기물이 정상적으로 이동하였습니다.");
+            return Result.onSuccess(ChessConverter.createmovePieceDto(request.getStartPos(), request.getEndPos()), MOVE_PIECE_SUCCESS);
         } catch (IllegalArgumentException e) {
             // 예외가 발생하면 Result 객체로 에러 메시지 반환
             return Result.onFailure(request, e.getMessage());
