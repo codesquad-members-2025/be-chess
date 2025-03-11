638aeaf57a78942ab88818a8ed3a52ef9ad9dd9e
jang-jinuk <dlsndifn98@gmail.com>
2025-03-11 14:44:32 +0900

    test: 체스 보드 초기화 시 검은색 폰 개수 검증 테스트 추가
    - 체스 보드가 초기화될 때 검은색 폰이 8개 존재하는지 확인하는 테스트 추가했습니다.
    - Board 클래스의 countPiece 메소드를 사용하여 검증합니다.

--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -1,7 +1,9 @@
 package chess;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pieces.Piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static utils.StringUtils.appendNewLine;
@@ -15,7 +17,7 @@ public class BoardTest {
     }
 
     @Test
-    public void create() throws Exception{
+    public void create() throws Exception {
         //Given & When
         String blankRank = appendNewLine("........");
         String expectedBoard = appendNewLine("RNBQKBNR") +
@@ -31,5 +33,21 @@ public class BoardTest {
         //Then
         assertThat(pieceCount).isEqualTo(32);
         assertThat(actualBoard).isEqualTo(expectedBoard);
+    }
+
+    @Test
+    @DisplayName("체스 보드 초기화 시 검은색 폰 8개가 존재해야 한다")
+    void countPiece() {
+        //Given
+        board.initialize();
+        Piece.Color color = Piece.Color.BLACK;
+        Piece.Type type = Piece.Type.PAWN;
+        int expectedCount = 8;
+
+        //When
+        int actualCount = board.countPiece(color, type);
+
+        //Then
+        assertThat(actualCount).isEqualTo(expectedCount);
     }
 }
