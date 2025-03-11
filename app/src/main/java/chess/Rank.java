56775a3a699f32b4a0d6c67957e68996ca75a52b
jang-jinuk <dlsndifn98@gmail.com>
2025-03-11 11:50:12 +0900

    feature: addPiece 메소드 구현
    기물을 랭크에 추가하는 메소드르 구현하였습니다.

--- app/src/main/java/chess/Rank.java
+++ app/src/main/java/chess/Rank.java
@@ -7,12 +7,16 @@ import java.util.ArrayList;
 public class Rank {
     private static final int MAX_RANK = 8;
 
-    ArrayList<Piece> rank;
+    private ArrayList<Piece> rank;
 
     public Rank() {
         this.rank = new ArrayList<>();
         for (int i = 0; i < MAX_RANK; ++i) {
             rank.add(new Piece(Piece.Color.NO_COLOR, Piece.Type.NO_PIECE));
         }
+    }
+
+    public void addPiece(Piece piece) {
+        rank.add(piece);
     }
 }
