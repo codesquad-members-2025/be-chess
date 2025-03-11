0ceec0b40d116a9a9d3e3249c9389609031dd35c
moongyuxx <gyuwon822@gmail.com>
2025-03-11 14:47:22 +0900

    main() 메소드에 move 명령 추가 * 이동을 완료한 후 체스 판의 상태를 콘솔에 출력

--- app/src/main/java/chess/ChessGame.java
+++ app/src/main/java/chess/ChessGame.java
@@ -5,21 +5,38 @@ import java.util.Scanner;
 public class ChessGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Board board = new Board();
+        BoardView boardView = new BoardView(board);
+        board.initialize();
 
         while (true) {
-            System.out.println ("Please enter \"start\" to start Ches Game and \"end\" to finish.");
+            System.out.println ("Please enter \"start\" to start Chess Game, \"move <source> <target>\" to move a piece, or \"end\" to finish.");
             String input = sc.nextLine();
+
             if (input.equals("start")) {
-                Board board = new Board();
-                BoardView boardView = new BoardView(board);
-                board.initialize();
                 System.out.println(boardView.showBoard());
-            } else if (input.equals("end")) {
+                continue;
+            }
+            if (input.equals("end")) {
                 break;
-            } else {
-                System.out.println ("Please enter a valid command.");
+            }
+            if (input.startsWith("move")) {
+                movePiece(board, input);
+                System.out.println(boardView.showBoard());
+                continue;
             }
 
+            System.out.println("Please enter a valid command.");
+
+        }
+    }
+
+    public static void movePiece(Board board, String input) {
+        String[] command = input.split(" ");
+        if (command.length == 3) {
+            board.move(command[1], command[2]);
+        } else {
+            System.out.println("Invalid move command. Use: move <source> <target>");
         }
     }
 }
