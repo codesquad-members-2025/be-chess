2aa36237ffe69c2343e5217d429ff1a538971a40
jang-jinuk <dlsndifn98@gmail.com>
2025-03-11 10:28:12 +0900

    feature: Rank 클래스 추가
    - 체스판 랭크를 관리해줄 Rank 클래스 도입하였습니다.

---
+++ app/src/main/java/chess/Rank.java
@@ -0,0 +1,13 @@
+package chess;
+
+import pieces.Piece;
+
+import java.util.ArrayList;
+
+public class Rank {
+    ArrayList<Piece> rank;
+
+    public Rank() {
+        this.rank = new ArrayList<>();
+    }
+}
