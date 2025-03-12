3d7c63240871d95dd2be9237f71aa615e119a16e
dorkem <chlwogur1000@gmail.com>
2025-03-12 17:53:08 +0900

    :sparkles: King 이동방향 체크
    King 주변으로 1칸씩 8방향 모두 갈 수 있으므로, everyDirection()를 가져옴.
    
    그래서 이동방향을 체크하고 해당 기물의 target 지점에 적이거나, 다른 기물에 막히지 않으면 true를 반환함

--- app/src/main/java/pieces/King.java
+++ app/src/main/java/pieces/King.java
@@ -1,5 +1,8 @@
 package pieces;
 
+import chess.Board;
+import java.util.List;
+
 public class King extends Piece {
     private King(Color color, Position position) {
         super(color, Type.KING, position);
@@ -10,7 +13,33 @@ public class King extends Piece {
     }
 
     @Override
-    public boolean canMove(Position target) {
+    public boolean canMove(Position target, Board board) {
+        Position cur = getPosition();
+        for (Direction direction : getDirections()) {
+            Position next = cur.move(direction);
+            if (!isValid(next)){
+                continue;
+            }
+
+            if (next.equals(target)) {
+                return isMatchPosition(target, board);
+            }
+        }
         return false;
+    }
+
+    private boolean isMatchPosition(Position target, Board board){
+        Piece targetPiece = board.findPiece(target);
+        return targetPiece.getType().equals(Type.NO_PIECE) || !targetPiece.getColor().equals(getColor());
+    }
+
+    private boolean isValid(Position position) {
+        return position.getX() >= 0 && position.getX() < 8 &&
+                position.getY() >= 0 && position.getY() < 8;
+    }
+
+    @Override
+    public List<Direction> getDirections() {
+        return Direction.everyDirection();
     }
 }
