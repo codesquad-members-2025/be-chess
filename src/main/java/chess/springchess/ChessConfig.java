9bfa274128d1c8655c14a38773abcfff089da572
jangsubin <eltm521@naver.com>
2025-03-12 10:29:43 +0900

    refactor : 사용하지 않는 import 삭제

--- src/main/java/chess/springchess/ChessConfig.java
+++ src/main/java/chess/springchess/ChessConfig.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ChessConfig {
