2c623ede6a6b91f00c9cec94fd297c25c823299a
dorkem <chlwogur1000@gmail.com>
2025-03-12 17:50:41 +0900

    :sparkles: Queen 이동방향 체크
    Queen은 팔방으로 이동하기 때문에 Direction의 everyDirection()를 가져옴.
    
    그래서 이동방향을 체크하고 해당 기물의 target 지점에 적이거나, 다른 기물에 막히지 않으면 true를 반환함

--- app/src/main/java/pieces/Queen.java
+++ app/src/main/java/pieces/Queen.java
@@ -1,5 +1,8 @@
 package pieces;
 
+import chess.Board;
+import java.util.List;
+
 public class Queen extends Piece {
     private Queen(Color color, Position position) {
         super(color, Type.QUEEN, position);
@@ -10,7 +13,43 @@ public class Queen extends Piece {
     }
 
     @Override
-    public boolean canMove(Position target) {
+    public boolean canMove(Position target, Board board) {
+        for (Direction direction : getDirections()) {
+            Position cur = getPosition();
+
+            while(true) {
+                Position next = cur.move(direction);
+
+                if (!isValid(next)) {
+                    break;
+                }
+
+                if (next.equals(target)) {
+                    return isMatchPosition(target, board);
+                }
+
+                if (board.findPiece(next).getType() != Type.NO_PIECE) {
+                    break;
+                }
+
+                cur = next;
+            }
+        }
         return false;
+    }
+
+    private boolean isValid(Position position) {
+        return position.getX() >= 0 && position.getX() < 8 &&
+                position.getY() >= 0 && position.getY() < 8;
+    }
+
+    private boolean isMatchPosition(Position target, Board board){
+        Piece targetPiece = board.findPiece(target);
+        return targetPiece.getType().equals(Type.NO_PIECE) || !targetPiece.getColor().equals(this.getColor());
+    }
+
+    @Override
+    public List<Direction> getDirections() {
+        return Direction.everyDirection();
     }
 }
