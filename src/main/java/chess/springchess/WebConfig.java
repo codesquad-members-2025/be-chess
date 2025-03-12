f58e098a53f6fab3b9a4291add97bd397d4bd649
jangsubin <eltm521@naver.com>
2025-03-12 17:13:08 +0900

    feat : CORS 수정 - 의미없는 설정 제거

--- src/main/java/chess/springchess/WebConfig.java
+++ src/main/java/chess/springchess/WebConfig.java
@@ -4,15 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Spring Boot의 경우
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
