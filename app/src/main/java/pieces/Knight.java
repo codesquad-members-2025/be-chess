77332ed46ad34c09dd469b9cc9b0b843ac51294a
dorkem <chlwogur1000@gmail.com>
2025-03-12 17:54:31 +0900

    :sparkles: Knight 이동방향 체크
    Knight는 기물을 뛰어넘을 수 있기 때문에 타겟 지점이 비어있거나 적 기물이면 이동이 가능합니다.

--- app/src/main/java/pieces/Knight.java
+++ app/src/main/java/pieces/Knight.java
@@ -1,5 +1,8 @@
 package pieces;
 
+import chess.Board;
+import java.util.List;
+
 public class Knight extends Piece {
     private Knight(Color color, Position position) {
         super(color, Type.KNIGHT, position);
@@ -10,7 +13,33 @@ public class Knight extends Piece {
     }
 
     @Override
-    public boolean canMove(Position target) {
+    public boolean canMove(Position target, Board board) {
+        Position cur = getPosition();
+        for(Direction direction : Direction.values()) {
+            Position next  = cur.move(direction);
+            if (!isValid(next)){
+                continue;
+            }
+
+            if (next.equals(target)) {
+                return isMatchPosition(target, board);
+            }
+        }
         return false;
+    }
+
+    private boolean isMatchPosition(Position target, Board board){
+        Piece targetPiece = board.findPiece(target);
+        return targetPiece.getType().equals(Type.NO_PIECE) || !targetPiece.getColor().equals(getColor());
+    }
+
+    private boolean isValid(Position position) {
+        return position.getX() >= 0 && position.getX() < 8 &&
+                position.getY() >= 0 && position.getY() < 8;
+    }
+
+    @Override
+    public List<Direction> getDirections() {
+        return Direction.knightDirection();
     }
 }
