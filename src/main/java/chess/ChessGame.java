91c4a10ae015ce3989d6a8adb4257c550dc14641
johnnie <come753zz@gmail.com>
2025-03-12 12:46:57 +0900

    main 메소드 작성

--- src/main/java/chess/ChessGame.java
+++ src/main/java/chess/ChessGame.java
@@ -1,6 +1,8 @@
 package chess;
 
 import chess.board.Board;
+import chess.config.ChessConfig;
+import chess.controller.ChessController;
 import chess.enums.Color;
 import chess.game.Game;
 import chess.view.ChessView;
@@ -12,8 +14,8 @@ import static chess.enums.Color.WHITE;
 public class ChessGame {
 
     public static void main(String[] args) {
-
-
+        ChessController controller =new ChessConfig().chessController();
+        controller.run();
     }
 }
 
