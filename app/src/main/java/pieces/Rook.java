6c4d4f5a7d11ae67256340d632225ada7318420c
dorkem <chlwogur1000@gmail.com>
2025-03-12 17:49:23 +0900

    :sparkles: Rook 이동방향 체크
    Rook은 일자로 이동하기 때문에 Direction의 linearDirection()를 가져옴.
    
    그래서 이동방향을 체크하고 해당 기물의 target 지점에 적이거나, 다른 기물에 막히지 않으면 true를 반환함

--- app/src/main/java/pieces/Rook.java
+++ app/src/main/java/pieces/Rook.java
@@ -1,5 +1,8 @@
 package pieces;
 
+import java.util.List;
+import chess.Board;
+
 public class Rook extends Piece {
     private Rook(Color color, Position position) {
         super(color, Type.ROOK, position);
@@ -10,10 +13,42 @@ public class Rook extends Piece {
     }
 
     @Override
-    public boolean canMove(Position target) {
-        Position position = getPosition();
-        if (position.getX() == target.getX() || position.getY() == target.getY()) { return true; }
+    public boolean canMove(Position target, Board board) {
+        for (Direction dir : getDirections()) {
+            Position cur = getPosition();
 
+            while (true) {
+                Position next = cur.move(dir);
+                if (!isValid(next)) {
+                    break;
+                }
+
+                if (next.equals(target)) {
+                    return isMatchPosition(target, board);
+                }
+
+                if (!next.equals(getPosition()) && board.findPiece(next).getType() != Type.NO_PIECE) {
+                    break;
+                }
+
+                cur = next;
+            }
+        }
         return false;
+    }
+
+    private boolean isValid(Position position) {
+        return position.getX() >= 0 && position.getX() < 8 &&
+                position.getY() >= 0 && position.getY() < 8;
+    }
+
+    private boolean isMatchPosition(Position target, Board board) {
+        Piece targetPiece = board.findPiece(target);
+        return targetPiece.getType().equals(Type.NO_PIECE) || !targetPiece.getColor().equals(this.getColor());
+    }
+
+    @Override
+    public List<Direction> getDirections() {
+        return Direction.linearDirection();
     }
 }
