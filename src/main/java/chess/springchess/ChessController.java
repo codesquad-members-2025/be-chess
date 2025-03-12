339fa9e24e7c8237c212d9612c95afc2ccf8b065
jangsubin <eltm521@naver.com>
2025-03-12 10:28:41 +0900

    feat : 기물 이동 시 킹을 잡았는지 확인 - 킹을 잡은 경우 다른 메세지를 보내도록 함

--- src/main/java/chess/springchess/ChessController.java
+++ src/main/java/chess/springchess/ChessController.java
@@ -3,6 +3,7 @@ package chess.springchess;
 import chess.game.Board;
 import chess.game.ChessGame;
 import chess.game.ChessView;
+import chess.pieces.Piece;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChessController {
     private static final String INIT_BOARD = "보드판이 초기화 되었습니다.";
     private static final String MOVE_PIECE_SUCCESS = "기물이 정상적으로 이동하였습니다.";
+    private static final String CATCH_KING_SUCCESS = "킹을 잡았습니다.";
     private final ChessGame chessGame;
     private final Board board;
     private final ChessView chessView;
@@ -31,7 +33,10 @@ public class ChessController {
     @PostMapping("api/move")
     public Result<ChessDto.movePieceDto> movePiece(@RequestBody ChessDto.movePieceDto request) {
         try {
-            chessGame.move(request.getStartPos(), request.getEndPos());
+            Piece move = chessGame.move(request.getStartPos(), request.getEndPos());
+            boolean kingOnBoard = chessGame.checkKingOnBoard(move);
+            if (!kingOnBoard)
+                return Result.onSuccess(ChessConverter.createmovePieceDto(request.getStartPos(), request.getEndPos()), CATCH_KING_SUCCESS);
             return Result.onSuccess(ChessConverter.createmovePieceDto(request.getStartPos(), request.getEndPos()), MOVE_PIECE_SUCCESS);
         } catch (IllegalArgumentException e) {
             // 예외가 발생하면 Result 객체로 에러 메시지 반환
