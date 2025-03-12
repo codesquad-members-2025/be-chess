97dc0fdd2af23e929917aa835d8017d7b38a997f
dorkem <chlwogur1000@gmail.com>
2025-03-12 21:25:27 +0900

    :recycle: 출력 리펙토링

--- app/src/main/java/chess/ChessView.java
+++ app/src/main/java/chess/ChessView.java
@@ -4,6 +4,8 @@ import static utils.StringUtils.appendNewLine;
 
 public class ChessView {
     private final Board board;
+    private final int RANK_SIZE = 8;
+
     public ChessView(Board board) {
         this.board = board;
     }
@@ -11,8 +13,16 @@ public class ChessView {
     public void showBoard(){
         StringBuilder answer = new StringBuilder();
 
-        for (Rank rank : board.getPieces()){
-            answer.append(appendNewLine(rank.toString()));
+        for (int i = 0; i < RANK_SIZE; i++) {
+
+            answer.append(board.getPieces().get(i).toString()).append("   ").append(appendNewLine(""+ (RANK_SIZE-i)));
+        }
+
+        answer.append(appendNewLine(""));
+
+        for (int i = 97; i < 105; i++){
+            String position = "" + (char) (i);
+            answer.append(position + " ");
         }
 
         System.out.println(answer);
