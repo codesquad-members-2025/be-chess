f17532749b7fe92f0defb163a2583b7513a40a38
moongyuxx <gyuwon822@gmail.com>
2025-03-13 12:51:27 +0900

    feat: King 클래스 추가

---
+++ app/src/main/java/pieces/King.java
@@ -0,0 +1,25 @@
+package pieces;
+
+import chess.Board;
+import chess.Position;
+
+import java.util.List;
+
+public class King extends Piece {
+    public King(Color color) {
+        super(color, Type.KING);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target, Board board) {
+        int rowDiff = Math.abs(source.getRow() - target.getRow());
+        int colDiff = Math.abs(source.getCol() - target.getCol());
+
+        if (rowDiff > 1 || colDiff > 1) {
+            return false;
+        }
+
+        isSameColorPiece(target, board);
+        return true;
+    }
+}
