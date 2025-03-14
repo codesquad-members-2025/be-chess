3f663eacbb5b04623809e0cf5e1048350f886e78
moongyuxx <gyuwon822@gmail.com>
2025-03-14 15:08:02 +0900

    feat: toString 오버라이드 추가

--- app/src/main/java/chess/Position.java
+++ app/src/main/java/chess/Position.java
@@ -4,9 +4,16 @@ public class Position {
     private final int row;
     private final int col;
 
+    //문자열 이용
     public Position(String position) { //a8
         this.col = position.charAt(0) - 'a'; //a->0, b -> 1, ...
         this.row = Character.getNumericValue(position.charAt(1)) - 1; //출력에서 뒤집어주기
+    }
+
+    //int값 이용 - isPathClear에 필요
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     public int getRow() {
@@ -15,5 +22,12 @@ public class Position {
 
     public int getCol() {
         return col;
+    }
+
+    @Override
+    public String toString() {
+        char colChar = (char) ('a' + col); // 0 → 'a', 1 → 'b', ...
+        int rowNum = row + 1; // 0-based index → 1-based
+        return String.valueOf(colChar) + rowNum;
     }
 }
