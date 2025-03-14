9247cf847dd2c77b0c4569ce6113efc7fe600e82
moongyuxx <gyuwon822@gmail.com>
2025-03-14 15:27:31 +0900

    refactor: King 움직임 Direction 이용하도록 수정

--- app/src/main/java/pieces/King.java
+++ app/src/main/java/pieces/King.java
@@ -1,6 +1,7 @@
 package pieces;
 
 import chess.Board;
+import chess.Direction;
 import chess.Position;
 
 import java.util.List;
@@ -12,17 +13,29 @@ public class King extends Piece {
 
     @Override
     public boolean canMove(Position source, Position target, Board board) {
-        int rowDiff = Math.abs(source.getRow() - target.getRow());
-        int colDiff = Math.abs(source.getCol() - target.getCol());
-
-        if (rowDiff > 1 || colDiff > 1) {
-            return false;
+        for (Direction direction : Direction.everyDirection()) {
+            if (canMoveRecursive(source, target, direction, board)) {
+                return true;
+            }
         }
-
-        if (isSameColorPiece(target, board)) {
-            return false;
-
-        }
-        return true;
+        return false;
     }
+
 }
+
+//    @Override
+//    public boolean canMove(Position source, Position target, Board board) {
+//        int rowDiff = Math.abs(source.getRow() - target.getRow());
+//        int colDiff = Math.abs(source.getCol() - target.getCol());
+//
+//        if (rowDiff > 1 || colDiff > 1) {
+//            return false;
+//        }
+//
+//        if (isSameColorPiece(target, board)) {
+//            return false;
+//
+//        }
+//        return true;
+//    }
+//}
--- app/src/test/java/pieces/KingTest.java
+++ app/src/test/java/pieces/KingTest.java
@@ -16,32 +16,33 @@ public class KingTest {
     }
 
     @Test
-    @DisplayName("King이 한 칸씩 이동할 수 있어야 한다")
-    public void moveKingOneStep() {
-        board.removePiece("e7"); // 이동할 위치를 비움
+    @DisplayName("King움직임 확인 - 8방향으로 한 칸만 움직일 수 있음")
+    public void moveKing() {
+        board.removePiece("e7"); // 이동할 위치를 제거 -> 나중에 PAWN 구현하면 PAWN 이동시키기
         Position source = new Position("e8");
         Position target = new Position("e7");
         Piece king = board.getPiece(source.getRow(), source.getCol());
 
-        //king이 이돌할 수 있는지만 검증 (실제 이동 x)
+        //king이 이돌할 수 있는지만 검증 (실제 이동 x, canMove만 확인)
         assertThat(king.canMove(source, target, board)).isTrue();
     }
 
     @Test
-    @DisplayName("King은 한 칸 이상 이동할 수 없다")
+    @DisplayName("King움직임 예외 처리 - 한 칸 이상 움지일 수 없음")
     public void invalidKingMove() {
         Position source = new Position("e8");
-        Position target = new Position("e6"); // 두 칸 이동 (잘못된 이동)
+        Position target = new Position("e6"); // 두 칸 이동
         Piece king = board.getPiece(source.getRow(), source.getCol());
 
         assertThat(king.canMove(source, target, board)).isFalse();
     }
 
+    //이 예외처리는 GameMove에서 해도 될 것 같음.
     @Test
-    @DisplayName("King이 같은 색 기물이 있는 곳으로 이동할 수 없다")
+    @DisplayName("King움직임 예외 처리 - 목적지에 같은 색 기물 있으면 이동 불가")
     public void kingCannotMoveToSameColorPiece() {
         Position source = new Position("e8");
-        Position target = new Position("e7"); // 이미 같은 색 기물이 있음
+        Position target = new Position("e7"); // 같은 색 기물
         Piece king = board.getPiece(source.getRow(), source.getCol());
 
         assertThat(king.canMove(source, target, board)).isFalse();
