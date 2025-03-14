674ada66928a7b1ee939e232dead9bd978b603c2
moongyuxx <gyuwon822@gmail.com>
2025-03-14 15:33:13 +0900

    refactor: Queen 이동 수정 * 재귀 이용

--- app/src/main/java/pieces/Queen.java
+++ app/src/main/java/pieces/Queen.java
@@ -3,30 +3,20 @@ package pieces;
 import chess.Board;
 import chess.Direction;
 import chess.Position;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Queen extends Piece {
     public Queen(Color color) {
-        super(color, Type.QUEEN);
+        super(color, Type.ROOK);
     }
 
     @Override
     public boolean canMove(Position source, Position target, Board board) {
-        List<Direction> allowedDirections = new ArrayList<>();
-        allowedDirections.addAll(Direction.linearDirection());
-        allowedDirections.addAll(Direction.diagonalDirection());
-
-        for (Direction direction : allowedDirections) {
-            int rowDiff = target.getRow() - source.getRow();
-            int colDiff = target.getCol() - source.getCol();
-
-
-            if (rowDiff % direction.getYDegree() == 0 && colDiff % direction.getXDegree() == 0) {
-                return isPathClear(source, target, board) && !isSameColorPiece(target, board);
+        for (Direction direction : Direction.everyDirection()) { // 룩은 직선 이동만 가능
+            if (canMoveRecursive(source, target, direction, board)) {
+                return true;
             }
         }
         return false;
     }
 }
-
