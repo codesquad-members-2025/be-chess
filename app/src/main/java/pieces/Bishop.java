29c72f19d4c3b24fec78cdfcb9f3852492ad3a0d
moongyuxx <gyuwon822@gmail.com>
2025-03-14 10:33:24 +0900

    feat: Bishop 이동 구현

---
+++ app/src/main/java/pieces/Bishop.java
@@ -0,0 +1,27 @@
+package pieces;
+
+import chess.Board;
+import chess.Direction;
+import chess.Position;
+import java.util.List;
+
+public class Bishop extends Piece {
+    public Bishop(Color color) {
+        super(color, Type.ROOK);
+    }
+
+    @Override
+    public boolean canMove(Position source, Position target, Board board) {
+        List<Direction> allowedDirections = Direction.diagonalDirection(); // 룩은 직선 이동만 가능
+
+        for (Direction direction : allowedDirections) {
+            int rowDiff = target.getRow() - source.getRow();
+            int colDiff = target.getCol() - source.getCol();
+
+            if (rowDiff % direction.getYDegree() == 0 && colDiff % direction.getXDegree() == 0) {
+                return isPathClear(source, target, board) && !isSameColorPiece(target, board);
+            }
+        }
+        return false; //이동 불가능한 경우
+    }
+}
